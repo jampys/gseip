c8f2c7d9d7fe6628ac8fdad95b1b674cb6897d5e
dario picon <dario@innertrip.com.ar>
2017-09-29 10:46:53 -0300

    GSEIP-17 se guardan cambios

--- view/objetivosForm.php
+++ view/objetivosForm.php
@@ -485,7 +485,7 @@
     </div>
 
 
-    <div class="form-group">
+    <div class="form-group required">
         <label class="col-md-4 control-label" for="descripcion">Meta</label>
             <div class="col-md-8">
                 <textarea class="form-control" name="meta" id="meta" placeholder="Meta" rows="2"><?php print $view->objetivo->getMeta(); ?></textarea>
@@ -493,7 +493,7 @@
     </div>
 
 
-    <div class="form-group">
+    <div class="form-group required">
         <label class="col-md-4 control-label" for="actividades">Actividades</label>
         <div class="col-md-8">
             <textarea class="form-control" name="actividades" id="actividades" placeholder="Actividades" rows="3"><?php print $view->objetivo->getActividades(); ?></textarea>
