3a10343acd2bebba5fa8957fbb8f33ee1459fdf2
dario <dario@innertrip.com.ar>
2017-11-29 11:00:27 -0300

    GSEIP-19 se guardan cambios

--- controller/uploadsController.php
+++ controller/uploadsController.php
@@ -53,22 +53,22 @@ switch ($operation) {
             //If Any browser does not support serializing of multiple files using FormData()
             if(!is_array($_FILES["myfile"]["name"])) //single file
             {
-                $fileName = $_FILES["myfile"]["name"];
-                if(move_uploaded_file($_FILES["myfile"]["tmp_name"],$output_dir.$fileName))
-                    RenovacionPersonal::uploadsUpload($output_dir, $fileName, $id); //inserta en la BD
-                $ret[]= $fileName;
-
+                $temp = explode(".", $_FILES["myfile"]["name"]);
+                $newfilename = $id . '_' . $temp[0] . '_' .round(microtime(true)) . '.' . end($temp);
+                if(move_uploaded_file($_FILES["myfile"]["tmp_name"], $output_dir . $newfilename))
+                    RenovacionPersonal::uploadsUpload($output_dir, $newfilename, $id); //inserta en la BD
+                $ret[]= $newfilename;
             }
             else  //Multiple files, file[]
             {
                 $fileCount = count($_FILES["myfile"]["name"]);
                 for($i=0; $i < $fileCount; $i++)
                 {
-                    $fileName = $_FILES["myfile"]["name"][$i];
-                    if(move_uploaded_file($_FILES["myfile"]["tmp_name"][$i],$output_dir.$fileName))
-                        RenovacionPersonal::uploadsUpload($output_dir, $fileName, $id); //inserta en la BD
-                    $ret[]= $fileName;
-
+                    $temp = explode(".", $_FILES["myfile"]["name"][$i]);
+                    $newfilename = $id . '_' . $temp[0] . '_' .round(microtime(true)) . '.' . end($temp);
+                    if(move_uploaded_file($_FILES["myfile"]["tmp_name"][$i], $output_dir . $newfilename))
+                        RenovacionPersonal::uploadsUpload($output_dir, $newfilename, $id); //inserta en la BD
+                    $ret[]= $newfilename;
                 }
 
             }
