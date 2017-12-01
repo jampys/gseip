58d2f30038b99cb2ee1cb9cd5229f5ee8456fd4f
dario <dario@innertrip.com.ar>
2017-12-01 09:06:54 -0300

    GSEIP-28 se guardan cambios

--- index.php
+++ index.php
@@ -1,45 +1,7 @@
 <?php
-/*
-session_start();
-require_once("lib/config1.php");
-require_once("model/seguridadModel.php");
-
-if(isset($_SESSION["ses_id"])){
-
-
-    if(!empty($_REQUEST["accion"])){
-        $accion=$_REQUEST["accion"];
-    }
-
-    if(is_file("controller/".$accion."Controller.php")){
-        require_once("controller/".$accion."Controller.php");
-    }else
-    {
-        require_once("controller/errorController.php");
-    }
-
-
-}else
-{
-    if($_GET["accion"]=="error"){
-        require_once("controller/errorController.php");
-    }
-    else{
-        require_once("controller/loginController.php");
-    }
-
-}
-
-require_once("view/layout.php");
-
-*/
 session_start();
 require_once("config/config.php");
 require_once("config/soporte.php");
-
-
-
-//require_once("controller/clientesController.php");
 
 
 if(isset($_SESSION["id_usuario"])){
