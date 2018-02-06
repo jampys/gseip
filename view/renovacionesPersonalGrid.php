f022e3b30e24912551471fd51bab93e4951e1276
dario <dario@innertrip.com.ar>
2018-02-06 10:48:06 -0300

    GSEIP-19 se guardan cambios

--- view/renovacionesPersonalGrid.php
+++ view/renovacionesPersonalGrid.php
@@ -37,6 +37,26 @@
         });
 
 
+        $(document).on("click", ".pdf", function(){
+            //var myWindow = window.open("", "myWindow", "width=200,height=100");
+            //myWindow.document.write("<p>This is 'myWindow'</p>");
+            //myWindow.opener.document.write("<p>This is the source window!</p>");
+            params={};
+            params.id_empleado = $('#search_empleado option:selected').attr('id_empleado');
+            params.id_grupo = $('#search_empleado option:selected').attr('id_grupo');
+            //var nro_version = Number($('#version').val());
+            //var lugar_trabajo = $('#lugar_trabajo').val();
+            //var usuario  = "<?php echo $_SESSION["USER_NOMBRE"].' '.$_SESSION["USER_APELLIDO"]; ?>";
+            //var id_cia = "<?php echo $_SESSION['ID_CIA']; ?>";
+            var strWindowFeatures = "location=yes,height=500,width=800,scrollbars=yes,status=yes, top=200,left=400";
+            //var URL="<?php echo $GLOBALS['ini']['report_url']; ?>frameset?__format=pdf&__report=sci_plan_version.rptdesign&p_periodo="+periodo+"&p_nro_version="+nro_version+"&p_lugar_trabajo="+lugar_trabajo+"&p_usuario="+usuario+"&p_id_cia="+id_cia;
+            var URL="<?php echo $GLOBALS['ini']['report_url']; ?>frameset?__format=pdf&__report=gseip_vencimientos.rptdesign&p_id_empleado="+params.id_empleado+"&p_nro_version="+params.id_empleado+"&p_lugar_trabajo="+params.id_empleado+"&p_usuario="+params.id_empleado+"&p_id_cia="+params.id_empleado;
+            var win = window.open(URL, "_blank", strWindowFeatures);
+            //alert(params.id_empleado);
+            return false;
+        });
+
+
 
 
 
@@ -110,6 +130,12 @@
                 <?php endforeach; ?>
             </tbody>
         </table>
+
+
+        <br/>
+        <div class="pull-right pdf">
+            <a href="index.php?action="><i class="far fa-file-pdf fa-fw fa-2x"></i></a>
+        </div>
 
     </div>
 
