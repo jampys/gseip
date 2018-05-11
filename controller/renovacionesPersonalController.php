3bde9fee3897dfe97e44f089a74d58749950dfe3
dario <dario@innertrip.com.ar>
2018-05-11 15:01:12 -0300

    HOTFIX-009 se guardan cambios

--- controller/renovacionesPersonalController.php
+++ controller/renovacionesPersonalController.php
@@ -16,7 +16,8 @@ switch ($operation)
         $view->disableLayout=true;
         $id_empleado = ($_POST['id_empleado']!='')? $_POST['id_empleado'] : null;
         $id_grupo = ($_POST['id_grupo']!='')? $_POST['id_grupo'] : null;
-        $id_vencimiento = ($_POST['id_vencimiento']!='')? $_POST['id_vencimiento'] : null;
+        //$id_vencimiento = ($_POST['id_vencimiento']!='')? $_POST['id_vencimiento'] : null;
+        $id_vencimiento = ($_POST['id_vencimiento']!='')? implode(",", $_POST['id_vencimiento'])  : 'vrp.id_vencimiento';
         $id_contrato = ($_POST['id_contrato']!='')? $_POST['id_contrato'] : null;
         $renovado = ($_POST['renovado']== 0)? null : 1;
         $view->renovaciones_personal = RenovacionPersonal::getRenovacionesPersonal($id_empleado, $id_grupo, $id_vencimiento, $id_contrato, $renovado);
