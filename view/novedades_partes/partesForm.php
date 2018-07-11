a2ceaf39cba39114d48b9ea27e0da2c9d270dd93
dario <dario@innertrip.com.ar>
2018-07-11 11:12:12 -0300

    GSEIP-46 se guardan cambios

--- view/novedades_partes/partesForm.php
+++ view/novedades_partes/partesForm.php
@@ -168,8 +168,7 @@
 
                     <?php foreach ($view->cuadrillas as $cu): ?>
                         <div class="row">
-                            <div class="col-md-1">hola</div>
-                            <div class="col-md-1">hola</div>
+                            <div class="col-md-2"><?php echo $cu['nombre'] ?></div>
                             <div class="col-md-2">
                                 <div class="form-group">
                                     <select class="selectpicker form-control">
@@ -179,18 +178,52 @@
                                     </select>
                                 </div>
                             </div>
-                            <div class="col-md-1"></div>
-                            <div class="col-md-1"></div>
-                            <div class="col-md-1"></div>
-                            <div class="col-md-1"></div>
-                            <div class="col-md-1"></div>
-                            <div class="col-md-1"></div>
+                            <div class="col-md-2">
+                                <div class="form-group">
+                                    <select class="selectpicker form-control">
+                                        <option>Mustard</option>
+                                        <option>Ketchup</option>
+                                        <option>Relish</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="col-md-2">
+                                <div class="form-group">
+                                    <select class="selectpicker form-control">
+                                        <option>Mustard</option>
+                                        <option>Ketchup</option>
+                                        <option>Relish</option>
+                                    </select>
+                                </div>
+                            </div>
+
+                            <div class="col-md-2">
+                                <div class="form-group">
+                                    <select class="selectpicker form-control">
+                                        <option>Mustard</option>
+                                        <option>Ketchup</option>
+                                        <option>Relish</option>
+                                    </select>
+                                </div>
+                            </div>
+
+                            <div class="col-md-1">
+                                <div class="form-group">
+                                    <select class="selectpicker form-control">
+                                        <option>Mustard</option>
+                                        <option>Ketchup</option>
+                                        <option>Relish</option>
+                                    </select>
+                                </div>
+                            </div>
+
+
+
                             <div class="col-md-1">
                                 <a class="<?php echo ( PrivilegedUser::dhasAction('ETP_UPDATE', array(1)) )? 'edit' : 'disabled' ?>" href="javascript:void(0);" title="editar">
                                     <span class="glyphicon glyphicon-edit" aria-hidden="true"></span>
                                 </a>
-                            </div>
-                            <div class="col-md-1">
+
                                 <a class="<?php echo ( PrivilegedUser::dhasAction('ETP_DELETE', array(1)) )? 'delete' : 'disabled' ?>" title="borrar" href="javascript:void(0);">
                                     <span class="glyphicon glyphicon-trash" aria-hidden="true"></span>
                                 </a>
