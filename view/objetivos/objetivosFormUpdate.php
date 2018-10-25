96ec21b69a2032f348a402154be9bd37f0804734
dario <dario@innertrip.com.ar>
2018-10-25 12:54:09 -0300

    GSEIP-51.3 se guardan cambios

--- view/objetivos/objetivosFormUpdate.php
+++ view/objetivos/objetivosFormUpdate.php
@@ -505,6 +505,25 @@
             <div class="modal-body">
 
 
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="alert alert-info">
+                            <p><strong> <?php print $view->objetivo->getCodigo() ?> </strong>
+                            <?php print $view->objetivo->getNombre() ?> </p>
+                        </div>
+                    </div>
+
+                    <div class="col-md-6">
+                        <div class="alert alert-info">
+                            <p><strong>Meta</strong> <?php print $view->objetivo->getMeta() ?>
+                            <strong>Indicador</strong> <?php print $view->objetivo->getIndicador() ?>
+                                <strong>Valor</strong> <?php print $view->objetivo->getMetaValor() ?>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+
 
                 <div id="chart_div"></div>
 
