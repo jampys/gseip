9ef1bf61c23b062e4862923efa09e28aad29366d
dario <dario@innertrip.com.ar>
2018-10-29 12:16:05 -0300

    GSEIP-51.4 saved

--- view/objetivos/objetivosGrid.php
+++ view/objetivos/objetivosGrid.php
@@ -69,8 +69,8 @@
 
 
                             <div class="progress" style="margin-bottom: 0px">
-                                <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;">
-                                    60%
+                                <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: <?php echo $rp['progreso']; ?>%;">
+                                    <?php echo $rp['progreso']; ?>%
                                 </div>
                             </div>
                         </td>
