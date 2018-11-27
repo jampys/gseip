1309f71609fdc6c6e3149954da09f1f72a26c5a0
dario <dario@innertrip.com.ar>
2018-11-27 16:57:32 -0300

    GSEIP-21 saved

--- view/evaluaciones/evaluaciones-eaoForm.php
+++ view/evaluaciones/evaluaciones-eaoForm.php
@@ -159,7 +159,6 @@
 
             if(jsonObjetivos[item.id_objetivo]) {
                 jsonObjetivos[item.id_objetivo].id_puntaje_objetivo = item.id_puntaje_objetivo;
-                jsonObjetivos[item.id_objetivo].ponderacion = item.ponderacion;
                 //alert('el elemento existe '+jsonObjetivos[item.id_competencia].id_puntaje);
             }
             else { //si no existe, lo agrega
