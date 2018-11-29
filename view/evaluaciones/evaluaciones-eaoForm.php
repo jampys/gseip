69ed6102ae57ffd8eecf1252c6c59bfd22fe7edf
dario <dario@innertrip.com.ar>
2018-11-29 12:04:09 -0300

    GSEIP-21.1 saved

--- view/evaluaciones/evaluaciones-eaoForm.php
+++ view/evaluaciones/evaluaciones-eaoForm.php
@@ -291,7 +291,7 @@
 
                                     <div class="col-md-2">
                                         <div class="input-group">
-                                            <input class="form-control ponderacion" type="text" name="ponderacion_<?php print $obj['id_objetivo']; ?>" value ="<?php print $obj['ponderacion']; ?>" >
+                                            <input class="form-control ponderacion" style="text-align: right" type="text" name="ponderacion_<?php print $obj['id_objetivo']; ?>" value ="<?php print $obj['ponderacion']; ?>" >
                                             <div class="input-group-addon">%</div>
                                         </div>
                                     </div>
