ede2309839340c60bdc6469ed2bddbb9a115b0d9
dario <dario@innertrip.com.ar>
2018-11-30 16:44:38 -0300

    GSEIP-67 saved

--- view/evaluaciones/evaluacionesLayout.php
+++ view/evaluaciones/evaluacionesLayout.php
@@ -83,11 +83,32 @@
             });
 
 
+
+
+
+
             //Al presionar en reporte
-            $(document).on('click', '.reporte', function(){
-                alert('Funcionalidad en desarrollo');
-                throw new Error();
+            $('#content').on("click", ".reporte", function(){
+                //$('.table-responsive').on("click", ".pdf", function(){
+                //alert('Funcionalidad en desarrollo');
+                //throw new Error();
+                params={};
+                params.id_empleado = $(this).closest('tr').attr('id_empleado');
+                params.periodo = $(this).closest('tr').attr('periodo');
+                //params.id_vencimiento = ($("#search_vencimiento").val()!= null)? $("#search_vencimiento").val() : '';
+                //params.renovado = $('#search_renovado').prop('checked')? 1 : '';
+                params.id_user = "<?php echo $_SESSION['id_user']; ?>";
+                //var strWindowFeatures = "location=yes,height=500,width=800,scrollbars=yes,status=yes, top=200,left=400";
+                var strWindowFeatures = "location=yes,height=500,width=800,scrollbars=yes,status=yes";
+                //var URL="<?php echo $GLOBALS['ini']['report_url']; ?>frameset?__format=pdf&__report=gseip_vencimientos_p.rptdesign&p_id_empleado="+params.id_empleado+"&p_id_grupo="+params.id_grupo+"&p_id_vencimiento="+params.id_vencimiento+"&p_id_contrato="+params.id_contrato+"&p_id_subcontratista="+params.id_subcontratista+"&p_renovado="+params.renovado+"&p_id_user="+params.id_user;
+                var URL="<?php echo $GLOBALS['ini']['report_url']; ?>frameset?__format=pdf&__report=gseip_ead_reporte_individual.rptdesign&p_id_empleado="+params.id_empleado+"&p_periodo="+params.periodo+"&p_id_user="+params.id_user;
+                //var win = window.open(URL, "_blank", strWindowFeatures);
+                var win = window.open(URL, "_blank");
+                return false;
             });
+
+
+
 
 
 
