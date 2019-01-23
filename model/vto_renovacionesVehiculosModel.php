db64297ce5364ef6fb66ecfa2a6ce4afdcb36649
dario <dario@innertrip.com.ar>
2019-01-23 12:59:23 -0300

    HOTFIX-13 saved

--- model/vto_renovacionesVehiculosModel.php
+++ model/vto_renovacionesVehiculosModel.php
@@ -255,6 +255,7 @@ order by priority, id_rnv_renovacion asc";
                                         :fecha_emision,
                                         :fecha_vencimiento,
                                         :referencia,
+                                        :created_by,
                                         @flag
                                     )';
 
@@ -266,6 +267,7 @@ order by priority, id_rnv_renovacion asc";
         $stmt->dpBind(':fecha_emision', $this->getFechaEmision());
         $stmt->dpBind(':fecha_vencimiento', $this->getFechaVencimiento());
         $stmt->dpBind(':referencia', $this->getReferencia());
+        $stmt->dpBind(':created_by', $this->getCreatedBy());
 
         $stmt->dpExecute();
 
