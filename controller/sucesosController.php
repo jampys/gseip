5889419f31f62b1387399bfe567ca07bac7a9bb8
dario <dario@innertrip.com.ar>
2019-04-10 15:43:48 -0300

    HOTFIX-047 saved

--- controller/sucesosController.php
+++ controller/sucesosController.php
@@ -106,25 +106,28 @@ switch ($operation)
             //$d = (string)$fh->diff($fd)->days;
             $d = (string)($fh->diff($fd)->days+1);
 
-            fwrite($handle, str_pad($su['txt_evento'], 10). //evento
-                            str_pad(substr($su['txt_legajo'], 2), 10). //legajo
-                            str_pad($fd->format('01/m/Y'), 10). //periodo desde
-                            str_pad($fh->format('01/m/Y'), 10). //periodo hasta
-                            str_pad($fd->format('d/m/Y'), 10). //fecha desde
-                            str_pad($fh->format('d/m/Y'), 10). //fecha hasta
-                            str_pad($d, 10). //dias
-                            str_pad($d, 10). //prorrateo dias
-                            str_pad("L", 10). //tipo liquidacion
-                            str_pad("MEN", 10). //tipo liquidacion
-                            str_pad("00/01/1900", 10). //fecha prevista notificacion
-                            str_pad("00/01/1900", 10). //fecha notificacion
-                            //str_pad(substr($su['observaciones'], 0, 10), 10). //observaciones
+            $line = str_pad($su['txt_evento'], 10). //evento
+                str_pad(substr($su['txt_legajo'], 2), 10). //legajo
+                str_pad($fd->format('01/m/Y'), 10). //periodo desde
+                str_pad($fh->format('01/m/Y'), 10). //periodo hasta
+                str_pad($fd->format('d/m/Y'), 10). //fecha desde
+                str_pad($fh->format('d/m/Y'), 10). //fecha hasta
+                str_pad($d, 10). //dias
+                str_pad($d, 10). //prorrateo dias
+                str_pad("L", 10). //tipo liquidacion
+                str_pad("MEN", 10). //tipo liquidacion
+                str_pad("00/01/1900", 10). //fecha prevista notificacion
+                str_pad("00/01/1900", 10). //fecha notificacion
+                //str_pad(substr($su['observaciones'], 0, 10), 10). //observaciones
+                "\r\n";
 
+            $line_no_bom = trim($line, "\\xef\\xbb\\xbf"); //remover el bom
 
-            "\r\n");
+            fwrite($handle, $line);
         }
 
         fclose($handle);
+        ob_end_clean(); //remover el bom
 
         header('Content-Type: application/octet-stream');
         header('Content-Disposition: attachment; filename='.basename($filepath));
