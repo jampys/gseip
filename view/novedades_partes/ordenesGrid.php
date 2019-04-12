7c42f0d7e91159d48d1145c1ff159b6db7f9dce2
dario <dario@innertrip.com.ar>
2019-04-12 17:15:35 -0300

    HOTFIX-048.1 saved

--- view/novedades_partes/ordenesGrid.php
+++ view/novedades_partes/ordenesGrid.php
@@ -27,13 +27,13 @@
                 </td>
 
                 <td class="text-center">
-                    <a class="<?php echo ( PrivilegedUser::dhasAction('ETP_UPDATE', array(1)) /*&& $et['id_user'] == $_SESSION['id_user']*/  )? 'edit' : 'disabled' ?>" href="javascript:void(0);" title="editar">
+                    <a class="<?php echo ( PrivilegedUser::dhasPrivilege('PAR_ABM', array(1)) /*&& $et['id_user'] == $_SESSION['id_user']*/  )? 'edit' : 'disabled' ?>" href="javascript:void(0);" title="editar">
                         <span class="glyphicon glyphicon-edit" aria-hidden="true"></span>
                     </a>
                 </td>
 
                 <td class="text-center">
-                    <a class="<?php echo ( PrivilegedUser::dhasAction('ETP_DELETE', array(1)) /*&& $et['id_user'] == $_SESSION['id_user'] */ )? 'delete' : 'disabled' ?>" title="borrar" href="javascript:void(0);">
+                    <a class="<?php echo ( PrivilegedUser::dhasPrivilege('PAR_ABM', array(1)) /*&& $et['id_user'] == $_SESSION['id_user'] */ )? 'delete' : 'disabled' ?>" title="borrar" href="javascript:void(0);">
                         <span class="glyphicon glyphicon-trash" aria-hidden="true"></span>
                     </a>
                 </td>
