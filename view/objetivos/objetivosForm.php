74f03b5f84c423ff82d5177bf6c48f5245ee616f
dario <dario@innertrip.com.ar>
2019-05-02 16:00:02 -0300

    HOTFIX-054 saved

--- view/objetivos/objetivosForm.php
+++ view/objetivos/objetivosForm.php
@@ -36,7 +36,8 @@
                 params.action = 'obj_objetivos';
                 params.operation = 'saveObjetivo';
                 params.id_objetivo=$('#id_objetivo').val();
-                params.periodo=$('#myModal #periodo').val();
+                params.periodo = $('#myModal #periodo').text();
+                params.id_plan_evaluacion =$('#myModal #periodo').val();
                 params.nombre=$('#nombre').val();
                 params.id_puesto=$('#id_puesto').val();
                 params.id_area=$('#id_area').val();
@@ -151,7 +152,7 @@
                         <select class="form-control selectpicker show-tick" id="periodo" name="periodo" title="Seleccione el periodo" data-live-search="true" data-size="5">
                             <?php foreach ($view->periodos as $pe){
                                 ?>
-                                <option value="<?php echo $pe['periodo']; ?>"
+                                <option value="<?php echo $pe['id_plan_evaluacion']; ?>"
                                     <?php echo (  ($view->objetivo->getPeriodo() == $pe['periodo']) ||  (!$view->objetivo->getPeriodo() && $pe['periodo'] == $view->periodo_actual)    )? 'selected' :'' ?>
                                     >
                                     <?php echo $pe['periodo']; ?>
