edbba508d1497f4f4ea3735f08809c874bc01f46
dario <dario@innertrip.com.ar>
2019-05-20 15:04:08 -0300

    HOTFIX-059.1 saved

--- view/novedades_partes/export_txtForm.php
+++ view/novedades_partes/export_txtForm.php
@@ -21,23 +21,76 @@
         $('#txt-form').validate({
             rules: {
                 id_contrato: {required: true},
-                fecha_desde: {required: true},
-                fecha_hasta: {required: true}
+                //fecha_desde: {required: true},
+                //fecha_hasta: {required: true}
+                id_periodo: {required: true}
             },
             messages:{
                 id_contrato: "Seleccione un contrato",
-                fecha_desde: "Seleccione la fecha desde",
-                fecha_hasta: "Seleccione la fecha hasta"
+                //fecha_desde: "Seleccione la fecha desde",
+                //fecha_hasta: "Seleccione la fecha hasta",
+                id_periodo: "Seleccione un período de liquidación"
 
             }
         });
 
 
-        $('.input-daterange').datepicker({ //ok
+        /*$('.input-daterange').datepicker({ //ok
             //todayBtn: "linked",
             format:"dd/mm/yyyy",
             language: 'es',
             todayHighlight: true
+        });*/
+
+
+        //Select dependiente: al seleccionar contrato carga periodos vigentes
+        $('#myModal').on('change', '#id_contrato', function(e){
+            //alert('seleccionó un contrato');
+            //throw new Error();
+            params={};
+            params.action = "nov_periodos";
+            params.operation = "getPeriodos";
+            //params.id_convenio = $('#id_parte_empleado option:selected').attr('id_convenio');
+            params.id_contrato = $('#id_contrato').val();
+            params.activos = 1;
+
+            $('#myModal #id_periodo').empty();
+
+
+            $.ajax({
+                url:"index.php",
+                type:"post",
+                //data:{"action": "parte-empleado-concepto", "operation": "getConceptos", "id_objetivo": <?php //print $view->objetivo->getIdObjetivo() ?>},
+                data: params,
+                dataType:"json",//xml,html,script,json
+                success: function(data, textStatus, jqXHR) {
+
+                    if(Object.keys(data).length > 0){
+
+                        $.each(data, function(indice, val){
+                            var label = data[indice]["nombre"]+' ('+data[indice]["fecha_desde"]+' - '+data[indice]["fecha_hasta"]+')';
+                            $("#myModal #id_periodo").append('<option value="'+data[indice]["id_periodo"]+'"'
+                            +' fecha_desde="'+data[indice]["fecha_desde"]+'"'
+                            +' fecha_hasta="'+data[indice]["fecha_hasta"]+'"'
+                            +'>'+label+'</option>');
+
+                        });
+
+                        //si es una edicion o view, selecciona el concepto.
+                        //$("#id_concepto").val(<?php //print $view->concepto->getIdConceptoConvenioContrato(); ?>);
+                        $('#myModal #id_periodo').selectpicker('refresh');
+
+                    }
+
+                },
+                error: function(data, textStatus, errorThrown) {
+                    //console.log('message=:' + data + ', text status=:' + textStatus + ', error thrown:=' + errorThrown);
+                    alert(data.responseText);
+                }
+
+            });
+
+
         });
 
 
@@ -100,21 +153,26 @@
                 //params.id_grupo = (typeof attr !== typeof undefined && attr !== false)? $('#search_empleado option:selected').attr('id_grupo') : '';
                 //params.id_vencimiento = $("#search_vencimiento").val();
                 //params.id_vencimiento = ($("#search_vencimiento").val()!= null)? $("#search_vencimiento").val() : '';
-                //params.id_subcontratista = $("#search_subcontratista").val();
                 //params.renovado = $('#search_renovado').prop('checked')? 1 : '';
 
                 params={};
-                params.action = 'partes';
-                params.operation = 'checkExportTxt';
-                params.fecha_desde = $("#myModal #fecha_desde").val();
-                params.fecha_hasta = $("#myModal #fecha_hasta").val();
+                //params.action = 'partes';
+                //params.operation = 'checkExportTxt';
+                //params.fecha_desde = $("#myModal #fecha_desde").val();
+                //params.fecha_hasta = $("#myModal #fecha_hasta").val();
                 params.id_contrato = $("#myModal #id_contrato").val();
+                params.id_periodo = $("#myModal #id_periodo").val();
                 params.id_user = "<?php echo $_SESSION['id_user']; ?>";
 
                 //var strWindowFeatures = "location=yes,height=500,width=800,scrollbars=yes,status=yes, top=200,left=400";
                 var strWindowFeatures = "location=yes,height=500,width=800,scrollbars=yes,status=yes";
-                //var URL="<?php //echo $GLOBALS['ini']['report_url']; ?>frameset?__format=pdf&__report=sci_plan_version.rptdesign&p_periodo="+periodo+"&p_nro_version="+nro_version+"&p_lugar_trabajo="+lugar_trabajo+"&p_usuario="+usuario+"&p_id_cia="+id_cia;
-                var URL="<?php echo $GLOBALS['ini']['report_url']; ?>frameset?__report=gseip_crossTab_novedades.rptdesign&p_id_contrato="+params.id_contrato+"&p_fecha_desde="+params.fecha_desde+"&p_fecha_hasta="+params.fecha_hasta+"&p_id_user="+params.id_user;
+                //var URL="<?php echo $GLOBALS['ini']['report_url']; ?>frameset?__report=gseip_crossTab_novedades.rptdesign&p_id_contrato="+params.id_contrato+"&p_fecha_desde="+params.fecha_desde+"&p_fecha_hasta="+params.fecha_hasta+"&p_id_user="+params.id_user;
+                var URL="<?php echo $GLOBALS['ini']['report_url']; ?>frameset?__report=gseip_crossTab_novedades.rptdesign&p_id_contrato="+params.id_contrato+
+                                                                    //"&p_fecha_desde="+params.fecha_desde+
+                                                                    //"&p_fecha_hasta="+params.fecha_hasta+
+                                                                    "&p_id_periodo="+params.id_periodo+
+                                                                    "&p_id_user="+params.id_user;
+
                 //var win = window.open(URL, "_blank", strWindowFeatures);
                 var win = window.open(URL, "_blank");
 
@@ -158,14 +216,14 @@
                     <br/>
 
 
-                    <div class="form-group required">
+                    <!--<div class="form-group required">
                         <label class="control-label" for="empleado">Fecha desde / hasta</label>
                         <div class="input-group input-daterange">
                             <input class="form-control" type="text" name="fecha_desde" id="fecha_desde" value = "<?php //print $view->contrato->getFechaDesde() ?>" placeholder="DD/MM/AAAA" readonly>
                             <div class="input-group-addon">a</div>
                             <input class="form-control" type="text" name="fecha_hasta" id="fecha_hasta" value = "<?php //print $view->contrato->getFechaHasta() ?>" placeholder="DD/MM/AAAA" readonly>
                         </div>
-                    </div>
+                    </div>-->
 
 
                     <div class="form-group required">
@@ -178,6 +236,14 @@
                                     <?php echo $con['nombre'].' '.$con['nro_contrato'];?>
                                 </option>
                             <?php  } ?>
+                        </select>
+                    </div>
+
+
+                    <div class="form-group required">
+                        <label for="id_periodo" class="control-label">Período de liquidación</label>
+                        <select class="form-control selectpicker show-tick" id="id_periodo" name="id_periodo" title="Seleccione un periodo" data-live-search="true" data-size="5">
+                            <!-- se completa dinamicamente desde javascript  -->
                         </select>
                     </div>
 
