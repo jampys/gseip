3909f02b2a556dd8d75af8141ab61af3700c2a34
dario <dario@innertrip.com.ar>
2019-05-22 13:36:32 -0300

    HOTFIX-059.1 saved

--- model/nov_partesModel.php
+++ model/nov_partesModel.php
@@ -270,30 +270,6 @@ join empleados em on em.id_empleado = npe.id_empleado
 join nov_parte_empleado_concepto npec on npec.id_parte_empleado = npe.id_parte_empleado
 join nov_concepto_convenio_contrato nccc on nccc.id_concepto_convenio_contrato = npec.id_concepto_convenio_contrato
 where np.id_contrato = :id_contrato
-and np.calculado is not null
-and np.fecha_parte between STR_TO_DATE(:fecha_desde, '%d/%m/%Y')
-and STR_TO_DATE(:fecha_hasta, '%d/%m/%Y')
-group by npe.id_empleado, nccc.codigo, nccc.variable
-UNION
-select em.legajo, nccc.codigo,
-sum(datediff(ns.fecha_hasta, ns.fecha_desde)+1) as cantidad,
-nccc.variable
-from nov_sucesos ns
-join empleados em on em.id_empleado = ns.id_empleado
-left join nov_convenios nc on nc.id_convenio = em.id_convenio
-join nov_eventos_l nel on nel.id_evento = ns.id_evento
-join nov_concepto_convenio_contrato nccc on nccc.id_concepto = nel.id_concepto and nccc.id_contrato = :id_contrato and nccc.id_convenio = em.id_convenio
-where ns.fecha_desde between STR_TO_DATE(:fecha_desde, '%d/%m/%Y')
-and STR_TO_DATE(:fecha_hasta, '%d/%m/%Y')
-and nccc.id_concepto in (15, 16)
-group by em.id_empleado, nccc.codigo, nccc.variable";*/
-        $query = "select em.legajo, nccc.codigo, sum(npec.cantidad) as cantidad, nccc.variable
-from nov_partes np
-join nov_parte_empleado npe on npe.id_parte = np.id_parte
-join empleados em on em.id_empleado = npe.id_empleado
-join nov_parte_empleado_concepto npec on npec.id_parte_empleado = npe.id_parte_empleado
-join nov_concepto_convenio_contrato nccc on nccc.id_concepto_convenio_contrato = npec.id_concepto_convenio_contrato
-where np.id_contrato = :id_contrato
 and np.last_calc_status is not null
 and np.id_periodo = :id_periodo
 group by npe.id_empleado, nccc.codigo, nccc.variable
@@ -308,7 +284,33 @@ join nov_eventos_l nel on nel.id_evento = ns.id_evento
 join nov_concepto_convenio_contrato nccc on nccc.id_concepto = nel.id_concepto and nccc.id_contrato = :id_contrato and nccc.id_convenio = em.id_convenio
 where (ns.id_periodo1 = :id_periodo or ns.id_periodo2 = :id_periodo)
 and nccc.id_concepto in (15, 16)
-group by em.id_empleado, nccc.codigo, nccc.variable";
+group by em.id_empleado, nccc.codigo, nccc.variable";*/
+        //Esta ultima version es similar al query anterior, solo se agrega un select de nivel superior para porder
+        //ordenarlo y que salga en el mismo orden que el pdf
+        $query = "select * from
+(select em.legajo, nccc.codigo, sum(npec.cantidad) as cantidad, nccc.variable, em.id_convenio
+from nov_partes np
+join nov_parte_empleado npe on npe.id_parte = np.id_parte
+join empleados em on em.id_empleado = npe.id_empleado
+join nov_parte_empleado_concepto npec on npec.id_parte_empleado = npe.id_parte_empleado
+join nov_concepto_convenio_contrato nccc on nccc.id_concepto_convenio_contrato = npec.id_concepto_convenio_contrato
+where np.id_contrato = :id_contrato
+and np.last_calc_status is not null
+and np.id_periodo = :id_periodo
+group by npe.id_empleado, nccc.codigo, nccc.variable
+UNION
+select em.legajo, nccc.codigo,
+(if(ns.id_periodo1 = :id_periodo, ifnull(ns.cantidad1,0), 0) + if(ns.id_periodo2 = :id_periodo, ifnull(ns.cantidad2,0), 0)) as cantidad,
+nccc.variable, em.id_convenio
+from nov_sucesos ns
+join empleados em on em.id_empleado = ns.id_empleado
+left join nov_convenios nc on nc.id_convenio = em.id_convenio
+join nov_eventos_l nel on nel.id_evento = ns.id_evento
+join nov_concepto_convenio_contrato nccc on nccc.id_concepto = nel.id_concepto and nccc.id_contrato = :id_contrato and nccc.id_convenio = em.id_convenio
+where (ns.id_periodo1 = :id_periodo or ns.id_periodo2 = :id_periodo)
+and nccc.id_concepto in (15, 16)
+group by em.id_empleado, nccc.codigo, nccc.variable) as temp
+order by id_convenio asc, legajo asc";
 
         $stmt->dpPrepare($query);
         $stmt->dpBind(':id_contrato', $id_contrato);
