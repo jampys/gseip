a55af32b5af275a9171e2f80120be54576f0d124
dario <dario@innertrip.com.ar>
2019-05-28 12:31:55 -0300

    HOTFIX-060 saved

--- view/evaluaciones/evaluacionesGrid.php
+++ view/evaluaciones/evaluacionesGrid.php
@@ -18,43 +18,6 @@
 
 
 
-        $('.table-responsive').on("click", ".gauss", function(e){
-            //e.preventDefault();
-            //alert('Funcionalidad en desarrollo');
-            //throw new Error();
-
-
-
-            params={};
-            params.action = "evaluaciones";
-            params.operation="loadGauss";
-            params.periodo = $("#periodo").val();
-            params.search_contrato = $("#search_contrato").val();
-            params.search_puesto = $('#search_puesto').val();
-            params.search_nivel_competencia = $('#search_nivel_competencia').val();
-            params.search_localidad = $('#search_localidad').val();
-            //params.id_vencimiento = ($("#search_vencimiento").val()!= null)? $("#search_vencimiento").val() : '';
-            //params.renovado = $('#search_renovado').prop('checked')? 1 : '';
-            //alert(params.search_contrato);
-
-
-            $('#popupbox').load('index.php', params,function(){
-                $('#myModal').modal();
-            });
-
-
-            //var nro_version = Number($('#version').val());
-
-            //var strWindowFeatures = "location=yes,height=500,width=800,scrollbars=yes,status=yes, top=200,left=400";
-            //var strWindowFeatures = "location=yes,height=500,width=800,scrollbars=yes,status=yes";
-            //var URL="<?php //echo $GLOBALS['ini']['report_url']; ?>frameset?__format=pdf&__report=gseip_vencimientos_v.rptdesign&p_id_vehiculo="+params.id_vehiculo+"&p_id_grupo="+params.id_grupo+"&p_id_vencimiento="+params.id_vencimiento+"&p_id_contrato="+params.id_contrato+"&p_id_subcontratista="+params.id_subcontratista+"&p_renovado="+params.renovado+"&p_id_user="+params.id_user;
-            //var win = window.open(URL, "_blank", strWindowFeatures);
-            //var win = window.open(URL, "_blank");
-            return false;
-        });
-
-
-
 
     });
 
@@ -156,12 +119,7 @@
             </tbody>
         </table>
 
-        <br/>
-        <div class="pull-right gauss">
-            <a href="#" title="Función de densidad">
-                <i class="fas fa-chart-area fa-fw fa-2x"></i>
-            </a>
-        </div>
+
 
     </div>
 
