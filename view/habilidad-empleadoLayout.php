88b5b0f2fae2e50aee6da826ec659af8bcf2f201
dario <dario@innertrip.com.ar>
2019-06-10 16:06:58 -0300

    HOTFIX-062 saved

--- view/habilidad-empleadoLayout.php
+++ view/habilidad-empleadoLayout.php
@@ -238,14 +238,14 @@
                         </div>
                         <div class="form-group col-md-2">
                             <label for="search">&nbsp;</label>
-                            <button type="button" class="form-control btn btn-primary btn-sm" id="search">
-                                <span class="glyphicon glyphicon-search fa-lg"></span>
+                            <button type="button" class="form-control btn btn-default btn-sm" id="search" title="Buscar">
+                                <span class="glyphicon glyphicon-search fa-lg dp_blue"></span>
                             </button>
                         </div>
                         <div class="form-group col-md-2">
                             <label for="search">&nbsp;</label>
-                            <button type="button" style="background-color: #337ab7" class="form-control btn btn-primary btn-sm" id="new" <?php echo ( PrivilegedUser::dhasAction('HEM_INSERT', array(1)) )? '' : 'disabled' ?> >
-                                <span class="glyphicon glyphicon-plus fa-lg"></span>
+                            <button type="button" class="form-control btn btn-default btn-sm" id="new" <?php echo ( PrivilegedUser::dhasAction('HEM_INSERT', array(1)) )? '' : 'disabled' ?> >
+                                <span class="glyphicon glyphicon-plus fa-lg dp_green"></span>
                             </button>
                         </div>
                     </form>
