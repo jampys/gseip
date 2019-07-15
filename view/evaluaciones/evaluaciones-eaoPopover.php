af7743c0d5a4a64aa1eac9008cb57635a703aade
dario <dario@innertrip.com.ar>
2019-07-15 14:13:16 -0300

    HOTFIX-068 saved

--- view/evaluaciones/evaluaciones-eaoPopover.php
+++ view/evaluaciones/evaluaciones-eaoPopover.php
@@ -5,7 +5,7 @@
     <div class="col-md-9">
 
         <div class="progress" style="margin-bottom: 0px">
-            <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: <?php echo ($obj['progreso'] <= 100)? $obj['progreso']:100; ?>%; min-width: 2em">
+            <div class="progress-bar progress-bar-striped active <?php echo Soporte::getProgressBarColor($obj['progreso']);?>" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: <?php echo ($obj['progreso'] <= 100)? $obj['progreso']:100; ?>%; min-width: 2em">
                 <?php echo $obj['progreso']; ?>%
             </div>
         </div>
