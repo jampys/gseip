4d79790ed05990189de103aa05ea94b44d7c3086
dario <dario@innertrip.com.ar>
2019-08-06 17:20:15 -0300

    HOTFIX-072 saved

--- view/habilitas/habilitasGrid.php
+++ view/habilitas/habilitasGrid.php
@@ -3,14 +3,9 @@
 
     $(document).ready(function(){
 
-        $(document).on('click', '#submit1',function(){
-            alert('toco en submit1');
-            window.location.href = "index.php?action=habilitas&operation=connection";
-            return false;
-        });
 
         $(document).on('click', '#culito',function(){
-            alert('toco en submit1');
+            //alert('toco en submit1');
             //window.location.href = "index.php?action=habilitas&operation=connection";
             $("#txt-form").submit(); // Submit the form
             return false;
@@ -57,21 +52,8 @@
 
 <div class="col-md-10">
 
-    <h4>Empleados</h4>
+    <h4>Convertidor</h4>
     <hr class="hr-primary"/>
-
-    <div style="text-align: right; margin-bottom: 10px">
-        <button class="btn btn-default" type="button" id="new" <?php echo ( PrivilegedUser::dhasAction('EMP_INSERT', array(1)) )? '' : 'disabled' ?> >
-            <span class="glyphicon glyphicon-plus dp_green" aria-hidden="true"></span> Nuevo Empleado
-        </button>
-    </div>
-
-
-
-
-
-
-
 
 
 
@@ -82,20 +64,29 @@
             <div class="alert alert-info" role="alert">
 
                 <div class="row">
-                    <div class="col-md-6">
+                    <div class="col-md-9">
                         <span class="glyphicon glyphicon-tags" ></span>&nbsp Muestra los partes involucrados para un período, empleado y concepto indicados.
                     </div>
-                    <div class="col-md-6">
-                        <!--<button class="btn btn-primary" id="submit1" name="submit1" type="submit">&nbsp;<i class="far fa-file-pdf fa-lg"></i>&nbsp;</button>-->
+                    <div class="col-md-3">
 
                             <label class="btn btn-primary" for="fileToUpload">
                                 <input id="fileToUpload" name="fileToUpload" type="file" style="display:none"
                                     onchange="$('#upload-file-info').html(this.files[0].name)">
                                 Seleccione archivo
                             </label>
-                            <span class='label label-info' id="upload-file-info"></span>
+
                             <button class="btn btn-primary" id="culito" name="culito" type="submit">Convertir</button>
 
+
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-md-9">
+
+                    </div>
+                    <div class="col-md-3">
+                        <span id="upload-file-info"></span>
 
                     </div>
                 </div>
@@ -104,7 +95,7 @@
     </form>
 
 
-            <div class="alert alert-info" role="alert">
+    <!--<div class="alert alert-info" role="alert">
                 <div class="row">
                     <div class="col-sm-10">
                         <span class="glyphicon glyphicon-tags" ></span>&nbsp Muestra los partes, tipos  y  números de órden para un período indicado.
@@ -113,24 +104,11 @@
                         <button class="btn btn-primary" id="submit2" name="submit2" type="submit">&nbsp;<i class="far fa-file-pdf fa-lg"></i>&nbsp;</button>
                     </div>
                 </div>
-            </div>
-
-
-            <div class="alert alert-info" role="alert">
-                <div class="row">
-                    <div class="col-sm-10">
-                        <span class="glyphicon glyphicon-tags" ></span>&nbsp Muestra los empleados sin parte ni suceso para un período indicado.
-                    </div>
-                    <div class="col-md-2">
-                        <button class="btn btn-primary" id="submit3" name="submit3" type="submit">&nbsp;<i class="far fa-file-pdf fa-lg"></i>&nbsp;</button>
-                    </div>
-                </div>
-            </div>
+            </div>-->
 
 
 
 
-        <div id="myElem" style="display:none"></div>
 
 
 
