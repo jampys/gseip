4acff9c70b89321c0b124a07c1325c31b39041d1
dario <dario@innertrip.com.ar>
2019-08-07 11:59:10 -0300

    HOTFIX-072 saved

--- view/habilitas/habilitasForm.php
+++ view/habilitas/habilitasForm.php
@@ -9,6 +9,17 @@
         });
 
 
+        $(document).on('click', '#clipboard',function(e){
+            var copyText = $("#descripcion");
+            /* Select the text field */
+            copyText.select();
+            /* Copy the text inside the text field */
+            document.execCommand("copy");
+            /* Alert the copied text */
+            //alert("Copied the text: " + copyText.value);
+        });
+
+
     });
 
 </script>
