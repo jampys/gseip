8ed7fcbc6d9dae6779cece256f4830fb5e45a4b6
dario <dario@innertrip.com.ar>
2019-09-12 16:29:04 -0300

    GSEIP-75 saved

--- view/usuarios/role_detailForm.php
+++ view/usuarios/role_detailForm.php
@@ -13,19 +13,19 @@
         });
 
 
-        /*$('.input-group.date').datepicker({
+        $('.input-group.date').datepicker({
             //inline: true
             format:"dd/mm/yyyy",
             language: 'es',
             todayHighlight: true
-        });*/
+        });
 
-        $('.input-daterange').datepicker({ //ok
+        /*$('.input-daterange').datepicker({
             //todayBtn: "linked",
             format:"dd/mm/yyyy",
             language: 'es',
             todayHighlight: true
-        });
+        });*/
 
 
         $('#myModal').modal({
@@ -95,8 +95,8 @@
         <strong><?php echo $view->label ?></strong>
     </div>
 
-    <input type="hidden" name="id_contrato_vehiculo" id="id_contrato_vehiculo" value="<?php print $view->contrato_vehiculo->getIdVehiculoContrato() ?>">
-    <input type="hidden" name="id_contrato" id="id_contrato" value="<?php print $view->contrato_vehiculo->getIdContrato() ?>">
+    <input type="hidden" name="id_contrato_vehiculo" id="id_contrato_vehiculo" value="<?php //print $view->contrato_vehiculo->getIdVehiculoContrato() ?>">
+    <input type="hidden" name="id_contrato" id="id_contrato" value="<?php //print $view->contrato_vehiculo->getIdContrato() ?>">
 
 
         <div class="form-group required">
@@ -106,7 +106,7 @@
                 <?php foreach ($view->vehiculos as $ar){ ?>
                     <option value="<?php echo $ar['id_vehiculo']; ?>"
                             data-content="<span class='label label-primary' style='font-weight: normal'><?php echo $ar['matricula']; ?></span> <span class='label label-default' style='font-weight: normal'><?php echo $ar['nro_movil']; ?></span> <?php echo $ar['modelo']; ?>"
-                        <?php echo ($ar['id_vehiculo'] == $view->contrato_vehiculo->getIdVehiculo())? 'selected' :'' ?>
+                        <?php //echo ($ar['id_vehiculo'] == $view->contrato_vehiculo->getIdVehiculo())? 'selected' :'' ?>
                         >
                         <?php //echo $ar['matricula'].' '.$ar['nro_movil'].' '.$ar['modelo']; ?>
                     </option>
@@ -115,27 +115,24 @@
         </div>
 
 
+
         <div class="form-group">
-            <label class="control-label" for="id_localidad" >Ubicación</label>
-            <select class="form-control selectpicker show-tick" id="id_localidad" name="id_localidad" title="Seleccione la ubicación" data-live-search="true" data-size="5">
-                <?php foreach ($view->localidades as $loc){
-                    ?>
-                    <option value="<?php echo $loc['id_localidad']; ?>"
-                        <?php echo ($loc['id_localidad'] == $view->contrato_vehiculo->getIdLocalidad())? 'selected' :'' ?>
-                        >
-                        <?php echo $loc['CP'].' '.$loc['ciudad'].' '.$loc['provincia'] ;?>
-                    </option>
-                <?php  } ?>
-            </select>
+            <label class="control-label" for="fecha">Fecha alta</label>
+            <div class="input-group date">
+                <input class="form-control" type="text" name="fecha_alta" id="fecha_alta" value = "<?php //print $view->usuario->getFechaAlta() ?>" placeholder="DD/MM/AAAA" disabled>
+                <div class="input-group-addon">
+                    <span class="glyphicon glyphicon-th"></span>
+                </div>
+            </div>
         </div>
 
-
-        <div class="form-group required">
-            <label class="control-label" for="empleado">Fecha desde / hasta</label>
-            <div class="input-group input-daterange">
-                <input class="form-control" type="text" name="fecha_desde" id="fecha_desde" value = "<?php print $view->contrato_vehiculo->getFechaDesde() ?>" placeholder="DD/MM/AAAA">
-                <div class="input-group-addon">a</div>
-                <input class="form-control" type="text" name="fecha_hasta" id="fecha_hasta" value = "<?php print $view->contrato_vehiculo->getFechaHasta() ?>" placeholder="DD/MM/AAAA">
+        <div class="form-group">
+            <label class="control-label" for="fecha">Fecha baja</label>
+            <div class="input-group date">
+                <input class="form-control" type="text" name="fecha_baja" id="fecha_baja" value = "<?php //print $view->usuario->getFechaBaja() ?>" placeholder="DD/MM/AAAA">
+                <div class="input-group-addon">
+                    <span class="glyphicon glyphicon-th"></span>
+                </div>
             </div>
         </div>
 
