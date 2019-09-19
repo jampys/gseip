7c66b68d7dd15ce565f0ed4c5d341b76496a868f
dario <dario@innertrip.com.ar>
2019-09-19 10:55:09 -0300

    HOTFIX-078 saved

--- view/busquedas/busquedasLayout.php
+++ view/busquedas/busquedasLayout.php
@@ -195,7 +195,7 @@
                             </select>
                     </div>
 
-                    <div class="form-group col-md-3">
+                    <div class="form-group col-md-2">
                         <label for="search_contrato" class="control-label">Contrato</label>
                         <select class="form-control selectpicker show-tick" id="search_contrato" name="search_contrato" data-live-search="true" data-size="5">
                             <option value="">Seleccione un contrato</option>
@@ -218,7 +218,7 @@
                         <button type="button" class="form-control btn btn-primary" id="new">Nueva renovación</button>
                     </div>-->
 
-                    <div class="form-group col-md-1" style="width: 11%">
+                    <div class="form-group col-md-2">
                         <label for="search_renovado" class="control-label">&nbsp;</label>
                         <div class="checkbox">
                             <label>
@@ -229,14 +229,14 @@
                     </div>
 
 
-                    <div class="form-group col-md-1" style="width: 7%">
+                    <div class="form-group col-md-1">
                         <label for="search">&nbsp;</label>
                         <button type="button" class="form-control btn btn-default" title="Buscar" id="search">
                             <span class="glyphicon glyphicon-search fa-lg dp_blue"></span>
                         </button>
                     </div>
 
-                    <div class="form-group col-md-1" style="width: 7%">
+                    <div class="form-group col-md-1">
                         <label for="search">&nbsp;</label>
                         <button type="button" class="form-control btn btn-default" title="nueva búsqueda" id="new" <?php echo ( PrivilegedUser::dhasAction('BUS_INSERT', array(1)) )? '' : 'disabled' ?>>
                             <span class="glyphicon glyphicon-plus fa-lg dp_green"></span>
