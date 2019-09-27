7dfd0a982ff74242f244506ba2f47156f20fe188
dario <dario@innertrip.com.ar>
2019-09-27 15:50:40 -0300

    HOTFIX-079 saved

--- view/busquedas/nPostulacion_detailForm.php
+++ view/busquedas/nPostulacion_detailForm.php
@@ -21,6 +21,8 @@
 
     $(document).ready(function(){
 
+        var nuevo = 0;
+
         //Necesario para que funcione el plug-in bootstrap-select
         $('.selectpicker').selectpicker({
             //propiedades del selectpicker
@@ -101,11 +103,13 @@
                     //$('#myModal').modal();
                     //$('#etapas_left_side #add').attr('id_busqueda', id);
                     $('.panel').show();
+                    nuevo = 1;
                 })
             }else{
                 //alert('cerrar');
                 $(this).removeClass("highlight");
                 $('.panel').hide();
+                nuevo = 0;
             }
 
         });
