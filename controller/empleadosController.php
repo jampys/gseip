5b34dca541b6131eecc217a7d5516e80eafabcbc
dario <dario@innertrip.com.ar>
2019-10-07 09:53:48 -0300

    HOTFIX-081 saved

--- controller/empleadosController.php
+++ controller/empleadosController.php
@@ -108,6 +108,98 @@ switch ($operation)
         break;
 
 
+
+    case 'saveVencimientos':
+
+        try{
+
+            sQuery::dpBeginTransaction();
+
+            $empleado = new Empleado($_POST['id_empleado']);
+            $id_empleado = $empleado->getIdEmpleado();
+
+            $vVencimientos = json_decode($_POST["vEmpleados"], true);
+            //print_r($vEmpleados);
+
+            foreach ($vEmpleados as $vE) {
+
+                //$c = new HabilidadEmpleado();
+                //$c->setIdHabilidad($vH['id_habilidad']);
+                //$c->setIdEmpleado($vE['id_empleado']);
+                //if($c->insertHabilidadEmpleado() < 0) $flag = -1;  //si falla algun insert $flag = -1
+
+                //echo "id_contrato :".$id." - id_empleado: ".$vE['id_empleado'];
+                //echo "id_contrato :".$id." - procesos: ".$vE['id_proceso'];
+                $empleado_contrato = new ContratoEmpleado();
+                $empleado_contrato->setIdEmpleadoContrato($vE['id_empleado_contrato']);
+                $empleado_contrato->setIdEmpleado($vE['id_empleado']);
+                $empleado_contrato->setIdContrato($id_contrato);
+                $empleado_contrato->setIdPuesto($vE['id_puesto']);
+                $empleado_contrato->setFechaDesde($vE['fecha_desde']);
+                $empleado_contrato->setFechaHasta(($vE['fecha_hasta'])? $vE['fecha_hasta']: null); //fecha_desde puede ser null
+                $empleado_contrato->setIdLocalidad(($vE['id_localidad'])? $vE['id_localidad']: null);
+
+                //echo 'id empleado contrato: '.$vE['id_empleado_contrato'].'---';
+
+                //echo $vE['operacion'];
+                if($vE['operacion']=='insert') {
+                    $empleado_contrato->insertEmpleadoContrato();
+                    $id_empleado_contrato = sQuery::dpLastInsertId();
+                    if($vE['id_proceso']){
+                        foreach($vE['id_proceso'] as $p){ //si se agregaron procesos
+                            //echo $p." ";
+                            $contrato_empleado_proceso = new ContratoEmpleadoProceso();
+                            $contrato_empleado_proceso->setIdEmpleadoContrato($id_empleado_contrato);
+                            $contrato_empleado_proceso->setIdProceso($p);
+                            $contrato_empleado_proceso->contratoEmpleadoProceso(); //si falla sale por el catch
+                        }
+
+                    }
+
+
+                }
+                else if( $vE['operacion']=='update') {
+                    $empleado_contrato->updateEmpleadoContrato();
+                    $id_empleado_contrato = $empleado_contrato->getIdEmpleadoContrato();
+                    if($vE['id_proceso']){ // si se editaron procesos
+                        foreach($vE['id_proceso'] as $p){
+                            //echo $p." ";
+                            $contrato_empleado_proceso = new ContratoEmpleadoProceso();
+                            $contrato_empleado_proceso->setIdEmpleadoContrato($id_empleado_contrato);
+                            $contrato_empleado_proceso->setIdProceso($p);
+                            $contrato_empleado_proceso->contratoEmpleadoProceso(); //si falla sale por el catch
+                            //echo 'operacion: '.$vE['operacion'].' - id_empleado_contrato: '.$contrato_empleado_proceso->getIdEmpleadoContrato().' - id_proceso: '.$contrato_empleado_proceso->getIdProceso();
+                        }
+
+                    }
+
+
+                }
+                else if( $vE['operacion']=='delete') {
+                    //Elimina en cascada los registros hijos de la tabla empleado_contrato_proceso
+                    $empleado_contrato->deleteEmpleadoContrato(); //si falla sale por el catch
+                }
+
+
+            }
+
+            //Devuelve el resultado a la vista
+            sQuery::dpCommit();
+            print_r(json_encode(1));
+
+        }
+        catch(Exception $e){
+            //echo $e->getMessage(); //habilitar para ver el mensaje de error
+            sQuery::dpRollback();
+            print_r(json_encode(-1));
+        }
+
+        exit;
+        break;
+
+
+
+
     default :
         $view->empleados=Empleado::getEmpleados();
         $view->contentTemplate="view/empleadosGrid.php"; // seteo el template que se va a mostrar
