57d81c7aeddb56e150d2355ddabc8841a649af8b
dario <dario@innertrip.com.ar>
2019-12-19 11:42:29 -0300

    GSEIP-75 saved

--- view/novedades2/3conceptoForm.php
+++ view/novedades2/3conceptoForm.php
@@ -27,158 +27,201 @@
 
 </script>
 
+<div class="row">
+
+    <div class="col-md-6" id="left_side">
+
+
+        <form name ="empleado-form" id="empleado-form" method="POST" action="index.php">
 
 
 
-<form name ="empleado-form" id="empleado-form" method="POST" action="index.php">
+
+            <div class="alert alert-info">
+                <strong><?php echo $view->label; ?></strong>
+            </div>
+
+
+            <!--<input type="hidden" name="id_parte" id="id_parte" value="<?php //print $view->empleado->getIdParte() ?>">-->
+            <input type="hidden" name="id_parte_empleado" id="id_parte_empleado" value="<?php //print $view->empleado->getIdParteEmpleado() ?>">
+
+
+
+            <div class="form-group">
+                <!--<label class="control-label" for="id_empleado">Cuadrilla</label>-->
+                <select id="id_cuadrilla" name="id_cuadrilla" class="form-control selectpicker show-tick" data-live-search="true" data-size="5" title="Seleccione una cuadrilla">
+                    <?php foreach ($view->cuadrillas as $cu){
+                        ?>
+                        <option value="<?php echo $cu['id_cuadrilla']; ?>"
+                            <?php //echo ($sup['codigo'] == $view->puesto->getCodigoSuperior())? 'selected' :'' ?>
+                            >
+                            <?php echo $cu['nombre']; ?>
+                        </option>
+                    <?php  } ?>
+                </select>
+            </div>
+
+            <div class="form-group">
+                <!--<label for="id_evento" class="control-label">Evento</label>-->
+                <select class="selectpicker form-control show-tick" id="id_evento" name="id_evento" data-live-search="true" data-size="5">
+                    <option value="">Seleccione un evento</option>
+                    <?php foreach ($view->eventos as $ar){ ?>
+                        <option value="<?php echo $ar['id_evento']; ?>"
+                            <?php //echo ($ar['id_evento'] == $view->parte->getIdEvento())? 'selected' :'' ?>
+                            >
+                            <?php echo $ar['codigo'].' '.$ar['nombre']; ?>
+                        </option>
+                    <?php  } ?>
+                </select>
+            </div>
+
+
+            <div class="form-group required">
+                <div class="checkbox">
+                    <label>
+                        <input type="checkbox" id="conductor" name="conductor" <?php //echo ($view->empleado->getConductor()== 1)? 'checked' :'' ?> <?php //echo (!$view->renovacion->getIdRenovacion())? 'disabled' :'' ?> > <a href="#" title="Marcar la persona que maneja">Conductor</a>
+                    </label>
+                </div>
+            </div>
+
+            <hr/>
+            <!-- SECCION DE CONCEPTOS -->
+
+            <div class="form-group">
+                <label for="id_evento" class="control-label">Conceptos</label>
+                <select class="selectpicker form-control show-tick" id="id_ruta" name="id_ruta" data-live-search="true" data-size="5">
+                    <option value="">Seleccione una ruta</option>
+                    <?php foreach ($view->eventos as $ar){ ?>
+                        <option value="<?php echo $ar['id_evento']; ?>"
+                            <?php //echo ($ar['id_evento'] == $view->parte->getIdEvento())? 'selected' :'' ?>
+                            >
+                            <?php echo $ar['codigo'].' '.$ar['nombre']; ?>
+                        </option>
+                    <?php  } ?>
+                </select>
+            </div>
+
+
+
+            <div class="row">
+
+
+                <div class="form-group col-md-6">
+                    <!-- <label for="add_contrato" class="control-label">Nuevos partes</label>-->
+                    <select class="form-control selectpicker show-tick" id="add_contrato" name="add_contrato" data-live-search="true" data-size="5">
+                        <option value="">Seleccione un concepto</option>
+                        <?php foreach ($view->contratos as $con){
+                            ?>
+                            <option value="<?php echo $con['id_contrato']; ?>" >
+                                <?php echo $con['nombre'].' '.$con['nro_contrato'];?>
+                            </option>
+                        <?php  } ?>
+                    </select>
+                </div>
+
+                <div class="form-group col-md-3">
+                    <!-- <label for="id_periodo" class="control-label">&nbsp;</label>-->
+                    <input class="form-control" type="text" name="referencia" id="referencia" value = "<?php //print $view->renovacion->getReferencia() ?>" placeholder="Cantidad">
+                </div>
+
+
+
+                <div class="form-group col-md-3">
+                    <!--<label for="search">&nbsp;</label>-->
+                    <button type="submit" class="form-control btn btn-default" title="nuevo parte" id="new" <?php echo ( PrivilegedUser::dhasAction('PAR_INSERT', array(1)) )? '' : 'disabled' ?>>
+                        <span class="glyphicon glyphicon-search fa-lg dp_blue"></span>
+                    </button>
+                </div>
+
+
+            </div>
 
 
 
 
-    <div class="alert alert-info">
-        <strong><?php echo $view->label; ?></strong>
+            <div class="table-responsive" id="empleados-table">
+                <table id="culin" class="table table-condensed dpTable table-hover">
+                    <thead>
+                    <tr>
+                        <th>Concepto</th>
+                        <th>Cant.</th>
+                        <th>Elim.</th>
+                        <th></th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <!-- se genera dinamicamente desde javascript -->
+                    </tbody>
+                </table>
+            </div>
+
+
+
+
+            <hr/>
+
+
+
+            <div class="form-group">
+                <!--<label class="control-label" for="servicio">Comentario</label>-->
+                <textarea class="form-control" name="comentario" id="comentario" placeholder="Comentario" rows="2"><?php //print $view->empleado->getComentario(); ?></textarea>
+            </div>
+
+
+
+            <div id="myElem" class="msg" style="display:none"></div>
+
+
+            <div id="footer-buttons" class="pull-right">
+                <button class="btn btn-primary" id="submit" name="submit" type="submit">Guardar</button>
+                <!--<button class="btn btn-default" id="cancel" name="cancel" type="button" data-dismiss="modal">Cancelar</button>-->
+                <button class="btn btn-default" id="cancel" name="cancel" type="button">Cancelar</button>
+            </div>
+
+
+
+        </form>
+
+
     </div>
 
 
-    <!--<input type="hidden" name="id_parte" id="id_parte" value="<?php //print $view->empleado->getIdParte() ?>">-->
-    <input type="hidden" name="id_parte_empleado" id="id_parte_empleado" value="<?php //print $view->empleado->getIdParteEmpleado() ?>">
+    <div class="col-md-6" id="right_side">
+        
 
+        <!-- seccion de ordenes -->
+        <div class="row">
+            <div class="col-md-4">
+                <button type="button" class="btn btn-primary btn-block" data-toggle="collapse" data-target="#demo-ordenes" title="Mostrar órdenes">Órdenes</button>
+            </div>
 
+            <div class="col-md-4">
 
-    <div class="form-group">
-        <!--<label class="control-label" for="id_empleado">Cuadrilla</label>-->
-        <select id="id_cuadrilla" name="id_cuadrilla" class="form-control selectpicker show-tick" data-live-search="true" data-size="5" title="Seleccione una cuadrilla">
-            <?php foreach ($view->cuadrillas as $cu){
-                ?>
-                <option value="<?php echo $cu['id_cuadrilla']; ?>"
-                    <?php //echo ($sup['codigo'] == $view->puesto->getCodigoSuperior())? 'selected' :'' ?>
-                    >
-                    <?php echo $cu['nombre']; ?>
-                </option>
-            <?php  } ?>
-        </select>
-    </div>
+            </div>
 
-    <div class="form-group">
-        <!--<label for="id_evento" class="control-label">Evento</label>-->
-        <select class="selectpicker form-control show-tick" id="id_evento" name="id_evento" data-live-search="true" data-size="5">
-            <option value="">Seleccione un evento</option>
-            <?php foreach ($view->eventos as $ar){ ?>
-                <option value="<?php echo $ar['id_evento']; ?>"
-                    <?php //echo ($ar['id_evento'] == $view->parte->getIdEvento())? 'selected' :'' ?>
-                    >
-                    <?php echo $ar['codigo'].' '.$ar['nombre']; ?>
-                </option>
-            <?php  } ?>
-        </select>
-    </div>
-
-
-    <div class="form-group required">
-        <div class="checkbox">
-            <label>
-                <input type="checkbox" id="conductor" name="conductor" <?php //echo ($view->empleado->getConductor()== 1)? 'checked' :'' ?> <?php //echo (!$view->renovacion->getIdRenovacion())? 'disabled' :'' ?> > <a href="#" title="Marcar la persona que maneja">Conductor</a>
-            </label>
+            <div class="col-md-4">
+                <button type="button" class="btn btn-default btn-block" id="add-orden" name="add-orden" title="Agregar orden" <?php echo ( PrivilegedUser::dhasPrivilege('PAR_ABM', array(1)) && $view->target!='view' )? '' : 'disabled' ?> >
+                    <i class="fas fa-plus dp_green"></i>&nbsp
+                </button>
+            </div>
         </div>
-    </div>
-
-    <hr/>
-    <!-- SECCION DE CONCEPTOS -->
-
-    <div class="form-group">
-        <label for="id_evento" class="control-label">Conceptos</label>
-        <select class="selectpicker form-control show-tick" id="id_ruta" name="id_ruta" data-live-search="true" data-size="5">
-            <option value="">Seleccione una ruta</option>
-            <?php foreach ($view->eventos as $ar){ ?>
-                <option value="<?php echo $ar['id_evento']; ?>"
-                    <?php //echo ($ar['id_evento'] == $view->parte->getIdEvento())? 'selected' :'' ?>
-                    >
-                    <?php echo $ar['codigo'].' '.$ar['nombre']; ?>
-                </option>
-            <?php  } ?>
-        </select>
-    </div>
 
 
-
-    <div class="row">
-
-
-        <div class="form-group col-md-6">
-            <!-- <label for="add_contrato" class="control-label">Nuevos partes</label>-->
-            <select class="form-control selectpicker show-tick" id="add_contrato" name="add_contrato" data-live-search="true" data-size="5">
-                <option value="">Seleccione un concepto</option>
-                <?php foreach ($view->contratos as $con){
-                    ?>
-                    <option value="<?php echo $con['id_contrato']; ?>" >
-                        <?php echo $con['nombre'].' '.$con['nro_contrato'];?>
-                    </option>
-                <?php  } ?>
-            </select>
-        </div>
-
-        <div class="form-group col-md-3">
-            <!-- <label for="id_periodo" class="control-label">&nbsp;</label>-->
-            <input class="form-control" type="text" name="referencia" id="referencia" value = "<?php //print $view->renovacion->getReferencia() ?>" placeholder="Cantidad">
-        </div>
-
-
-
-        <div class="form-group col-md-3">
-            <!--<label for="search">&nbsp;</label>-->
-            <button type="submit" class="form-control btn btn-default" title="nuevo parte" id="new" <?php echo ( PrivilegedUser::dhasAction('PAR_INSERT', array(1)) )? '' : 'disabled' ?>>
-                <span class="glyphicon glyphicon-search fa-lg dp_blue"></span>
-            </button>
+        <div id="demo-ordenes" class="collapse">
+            <div class="grid-ordenes">
+                <?php include_once('view/novedades_partes/ordenesGrid.php');?>
+            </div>
         </div>
 
 
     </div>
 
-
-
-
-    <div class="table-responsive" id="empleados-table">
-        <table id="culin" class="table table-condensed dpTable table-hover">
-            <thead>
-            <tr>
-                <th>Concepto</th>
-                <th>Cant.</th>
-                <th>Elim.</th>
-                <th></th>
-            </tr>
-            </thead>
-            <tbody>
-            <!-- se genera dinamicamente desde javascript -->
-            </tbody>
-        </table>
-    </div>
+</div>
 
 
 
 
-    <hr/>
 
-
-
-    <div class="form-group">
-        <!--<label class="control-label" for="servicio">Comentario</label>-->
-        <textarea class="form-control" name="comentario" id="comentario" placeholder="Comentario" rows="2"><?php //print $view->empleado->getComentario(); ?></textarea>
-    </div>
-
-
-
-    <div id="myElem" class="msg" style="display:none"></div>
-
-
-    <div id="footer-buttons" class="pull-right">
-        <button class="btn btn-primary" id="submit" name="submit" type="submit">Guardar</button>
-        <!--<button class="btn btn-default" id="cancel" name="cancel" type="button" data-dismiss="modal">Cancelar</button>-->
-        <button class="btn btn-default" id="cancel" name="cancel" type="button">Cancelar</button>
-    </div>
-
-
-
-</form>
 
 
 
---
+++ view/novedades2/ordenesGrid.php
@@ -0,0 +1,59 @@
+<?php if(isset($view->ordenes) && sizeof($view->ordenes) > 0) {?>
+
+    <table class="table table-condensed dataTable table-hover">
+        <thead>
+        <tr>
+            <!--<th>Nro. parte</th>
+            <th>Tipo orden</th>
+            <th>Nro. orden</th>
+            <th></th>
+            <th></th>
+            <th></th>-->
+        </tr>
+        </thead>
+        <tbody>
+        <?php foreach ($view->ordenes as $em): ?>
+            <tr data-id="<?php echo $em['id_parte_orden'];?>">
+                <td><?php echo $em['nro_parte_diario'];?></td>
+                <td><?php echo $em['orden_tipo'];?></td>
+                <td><?php echo $em['orden_nro'];?></td>
+                <!--<td style="text-align: center"><?php //echo($et['aplica'] == 1)? '<i class="far fa-thumbs-up fa-fw" style="color: #49ed0e"></i>':'<i class="far fa-thumbs-down fa-fw" style="color: #fc140c"></i>'; ?></td>
+                <td><?php //echo $et['user'];?></td>-->
+
+                <td class="text-center">
+                    <a class="view" href="javascript:void(0);" data-id="<?php //echo $et['id_etapa'];?>" title="ver">
+                        <span class="glyphicon glyphicon-eye-open dp_blue" aria-hidden="true"></span>
+                    </a>&nbsp;&nbsp;
+
+                    <a class="<?php echo ( PrivilegedUser::dhasPrivilege('PAR_ABM', array(1)) && $view->target!='view' /*&& $et['id_user'] == $_SESSION['id_user']*/  )? 'edit' : 'disabled' ?>" href="javascript:void(0);" title="editar">
+                        <span class="glyphicon glyphicon-edit dp_blue" aria-hidden="true"></span>
+                    </a>&nbsp;&nbsp;
+
+                    <a class="<?php echo ( PrivilegedUser::dhasPrivilege('PAR_ABM', array(1)) && $view->target!='view' /*&& $et['id_user'] == $_SESSION['id_user']*/  )? 'clone' : 'disabled' ?>" href="javascript:void(0);" title="clonar">
+                        <span class="glyphicon glyphicon-duplicate dp_blue">
+                    </a>&nbsp;&nbsp;
+
+                    <a class="<?php echo ( PrivilegedUser::dhasPrivilege('PAR_ABM', array(1)) && $view->target!='view' /*&& $et['id_user'] == $_SESSION['id_user'] */ )? 'delete' : 'disabled' ?>" title="borrar" href="javascript:void(0);">
+                        <span class="glyphicon glyphicon-trash dp_red" aria-hidden="true"></span>
+                    </a>
+                </td>
+            </tr>
+        <?php endforeach; ?>
+        </tbody>
+    </table>
+
+
+
+<?php }else{ ?>
+
+    <br/>
+    <div class="alert alert-warning">
+        <i class="fas fa-exclamation-triangle fa-fw"></i> El parte no tiene órdenes registradas.
+    </div>
+
+<?php } ?>
+
+
+
+
+
