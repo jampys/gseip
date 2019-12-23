c14ee524703c4b76592bb0617d7199ff8bc41d63
dario <dario@innertrip.com.ar>
2019-12-23 16:20:12 -0300

    GSEIP-75 saved

--- controller/novedades2Controller.php
+++ controller/novedades2Controller.php
@@ -67,7 +67,7 @@ switch ($operation)
         $view->eventos = EventosCuadrilla::getEventosCuadrilla();
         $view->ordenes = ParteOrden::getParteOrden($_POST['id_parte']); //2104
         $view->periodo = New NovPeriodo($_POST['id_periodo']);
-        $view->conceptos = New ParteEmpleadoConcepto($_POST['id_parte_empleado']);
+        $view->conceptos = ParteEmpleadoConcepto::getParteEmpleadoConcepto2($_POST['id_parte_empleado']);
 
         $eventos = ($_POST['eventos']!='')? implode(",", $_POST['eventos'])  : 'su.id_evento';
         $fecha_desde = $view->periodo->getFechaDesde(); //($_POST['fecha']!='')? $_POST['fecha'] : null;
