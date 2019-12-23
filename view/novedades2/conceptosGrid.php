10a8159834361408f9fa49c1d2d844c09e8690a0
dario <dario@innertrip.com.ar>
2019-12-23 17:14:23 -0300

    GSEIP-75 saved

--- view/novedades2/conceptosGrid.php
+++ view/novedades2/conceptosGrid.php
@@ -20,19 +20,18 @@
                     <td><?php echo $ctos['convenio'];?></td>
                     <td><?php echo $ctos['concepto'];?></td>
                     <td><?php echo $ctos['codigo'];?></td>
-                    <!--<td><?php //echo $ctos['variable'];?></td>-->
                     <td><?php echo $ctos['cantidad'];?></td>
                     <!--<td style="text-align: center"><?php //echo($et['aplica'] == 1)? '<i class="far fa-thumbs-up fa-fw" style="color: #49ed0e"></i>':'<i class="far fa-thumbs-down fa-fw" style="color: #fc140c"></i>'; ?></td>
                 <td><?php //echo $et['user'];?></td>-->
 
                     <td class="text-center">
-                        <a class="<?php echo (true && $ctos['tipo_calculo']=='M' )? 'view' : 'disabled' ?>" href="javascript:void(0);" data-id="<?php //echo $et['id_etapa'];?>" title="ver">
+                        <!--<a class="<?php //echo (true && $ctos['tipo_calculo']=='M' )? 'view' : 'disabled' ?>" href="javascript:void(0);" data-id="<?php //echo $et['id_etapa'];?>" title="ver">
                             <span class="glyphicon glyphicon-eye-open dp_blue" aria-hidden="true"></span>
-                        </a>&nbsp;&nbsp;
+                        </a>&nbsp;&nbsp;-->
 
-                        <a class="<?php echo (PrivilegedUser::dhasPrivilege('PAR_ABM', array(1)) && $view->target!='view' && $ctos['tipo_calculo']=='M')? 'edit' : 'disabled' ?>" href="javascript:void(0);" title="editar">
+                        <!--<a class="<?php //echo (PrivilegedUser::dhasPrivilege('PAR_ABM', array(1)) && $view->target!='view' && $ctos['tipo_calculo']=='M')? 'edit' : 'disabled' ?>" href="javascript:void(0);" title="editar">
                             <span class="glyphicon glyphicon-edit dp_blue" aria-hidden="true"></span>
-                        </a>&nbsp;&nbsp;
+                        </a>&nbsp;&nbsp;-->
 
                         <a class="<?php echo (PrivilegedUser::dhasPrivilege('PAR_ABM', array(1)) && $view->target!='view' && $ctos['tipo_calculo']=='M' /*&& $et['id_user'] == $_SESSION['id_user'] */ )? 'delete' : 'disabled' ?>" title="borrar" href="javascript:void(0);">
                             <span class="glyphicon glyphicon-trash dp_red" aria-hidden="true"></span>
