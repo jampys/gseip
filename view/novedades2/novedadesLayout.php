63db61b1b6a273755c5266a68d6b5e5b8befd248
dario <dario@innertrip.com.ar>
2020-01-10 14:15:18 -0300

    GSEIP-75 saved

--- view/novedades2/novedadesLayout.php
+++ view/novedades2/novedadesLayout.php
@@ -33,7 +33,7 @@
                 params.operation = "getPeriodosAndCuadrillas";
                 //params.id_convenio = $('#id_parte_empleado option:selected').attr('id_convenio');
                 params.id_contrato = $('#add_contrato').val();
-                params.activos = 1;
+                //params.activos = 1;
 
                 $('#id_periodo').empty();
                 $('#cuadrilla').empty();
