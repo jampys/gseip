901d53a22b27d702121adebd5b34944ece5633a2
dario <dario@innertrip.com.ar>
2020-01-13 12:53:53 -0300

    GSEIP-75.1 saved

--- view/novedades2/empleadosGrid.php
+++ view/novedades2/empleadosGrid.php
@@ -16,7 +16,7 @@
             >
             <td><a href="#"><?php echo $em['apellido'].' '.$em['nombre']; ?></a></td>
             <td style="text-align: center">
-                <?php echo($em['id_parte'] && $em['last_calc_status'])? '<i class="fa fa-car-side fa-fw dp_green" title="parte calculado"></i>':'<i class="fa fa-car fa-fw dp_yellow" title="parte sin calcular"></i>'; ?>&nbsp;&nbsp;
+                <?php echo($em['id_parte'])? '<i class="fa fa-car-side fa-fw dp_green" title="parte calculado"></i>':'<i class="fa fa-car fa-fw dp_yellow" title="parte sin calcular"></i>'; ?>&nbsp;&nbsp;
                 <?php echo($em['id_parte'] && $em['orden_count']>0)? '<i class="fas fa-clipboard-check fa-fw dp_green" title="parte con órdenes"></i>':'<i class="fas fa-clipboard fa-fw dp_yellow" title="parte sin órdenes"></i>'; ?>
             </td>
         </tr>
