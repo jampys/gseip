ecf360bd1a3eaf460094958ef6cbc3881b37da7d
dario <dario@innertrip.com.ar>
2020-01-17 15:24:50 -0300

    HOTFIX-087 saved

--- view/habilidad-puestoFormUpdate.php
+++ view/habilidad-puestoFormUpdate.php
@@ -99,7 +99,7 @@
 
 
             <div class="modal-footer">
-                <button class="btn btn-primary" id="submit" name="submit" type="submit">Guardar</button>
+                <button class="btn btn-primary" id="submit" name="submit" type="submit" <?php echo ( PrivilegedUser::dhasAction('HPU_UPDATE', array(1)) && $view->target!='view')? '' : 'disabled' ?> >Guardar</button>
                 <button class="btn btn-default" id="cancel" name="cancel" type="button" data-dismiss="modal">Cancelar</button>
             </div>
 
