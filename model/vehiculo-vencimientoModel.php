38fa1e97399deec29aff97185e3f9b0c03790c63
dario <dario@innertrip.com.ar>
2020-03-09 17:05:31 -0300

    GSEIP-78 saved

---
+++ model/vehiculo-vencimientoModel.php
@@ -0,0 +1,139 @@
+<?php
+
+class VehiculoVencimiento
+{
+    private $id_vehiculo_vencimiento;
+    private $id_vehiculo;
+    private $id_vencimiento;
+    private $created_date;
+
+    // GETTERS
+    function getIdVehiculoVencimiento()
+    { return $this->id_vehiculo_vencimiento;}
+
+    function getIdVehiculo()
+    { return $this->id_vehiculo;}
+
+    function getIdVencimiento()
+    { return $this->id_vencimiento;}
+
+    function getCreatedDate()
+    { return $this->created_date;}
+
+
+    //SETTERS
+    function setIdVehiculoVencimiento($val)
+    { $this->id_vehiculo_vencimiento=$val;}
+
+    function setIdVehiculo($val)
+    { $this->id_vehiculo=$val;}
+
+    function setIdVencimiento($val)
+    {  $this->id_vencimiento=$val;}
+
+    function setCreatedDate($val)
+    { $this->created_date=$val;}
+
+
+    function __construct($nro=0){ //constructor //ok
+
+        if ($nro!=0){
+            $stmt=new sQuery();
+            $query = "select id_vehiculo_vencimiento, id_vehiculo, id_vencimiento,
+                      DATE_FORMAT(created_date, '%d/%m/%Y') as created_date
+                      from vehiculo_vencimiento
+                      where id_vehiculo_vencimiento = :nro";
+            $stmt->dpPrepare($query);
+            $stmt->dpBind(':nro', $nro);
+            $stmt->dpExecute();
+            $rows = $stmt ->dpFetchAll();
+
+            $this->setIdVehiculoVencimiento($rows[0]['id_vehiculo_vencimiento']);
+            $this->setIdVehiculo($rows[0]['id_vehiculo']);
+            $this->setIdVencimiento($rows[0]['id_vencimiento']);
+            $this->setCreatedDate($rows[0]['created_date']);
+        }
+    }
+
+
+    public static function getEmpleadoVencimiento($id_empleado) { //ok
+        $stmt=new sQuery();
+        $query = "select v.id_vencimiento, v.nombre, ev.id_empleado_vencimiento, ev.id_empleado, ev.created_date
+from vto_vencimiento_p v
+left join empleado_vencimiento ev on v.id_vencimiento = ev.id_vencimiento and ev.id_empleado = :id_empleado
+order by v.nombre asc";
+        $stmt->dpPrepare($query);
+        $stmt->dpBind(':id_empleado', $id_empleado);
+        $stmt->dpExecute();
+        return $stmt->dpFetchAll();
+    }
+
+
+    /*function save(){
+        if($this->id_cuadrilla_empleado)
+        {$rta = $this->updateCuadrillaEmpleado();}
+        else
+        {$rta =$this->insertCuadrillaEmpleado();}
+        return $rta;
+    }*/
+
+
+    /*public function updateCuadrillaEmpleado(){
+        $stmt=new sQuery();
+        $query="update nov_cuadrilla_empleado set id_empleado = :id_empleado,
+                conductor = :conductor
+                where id_cuadrilla_empleado = :id_cuadrilla_empleado";
+        $stmt->dpPrepare($query);
+        $stmt->dpBind(':id_empleado', $this->getIdEmpleado());
+        $stmt->dpBind(':id_cuadrilla_empleado', $this->getIdCuadrillaEmpleado());
+        $stmt->dpBind(':conductor', $this->getConductor());
+        $stmt->dpExecute();
+        return $stmt->dpGetAffect();
+    }*/
+
+
+    public function insertEmpleadoVencimiento(){ //ok
+        $stmt=new sQuery();
+        $query="insert into empleado_vencimiento(id_empleado, id_vencimiento, created_date)
+                values(:id_empleado, :id_vencimiento, sysdate())";
+        $stmt->dpPrepare($query);
+        $stmt->dpBind(':id_empleado', $this->getIdEmpleado());
+        $stmt->dpBind(':id_vencimiento', $this->getIdVencimiento());
+        $stmt->dpExecute();
+        return $stmt->dpGetAffect();
+    }
+
+
+    public function deleteEmpleadoVencimiento(){ //ok
+        $stmt=new sQuery();
+        $query="delete from empleado_vencimiento where id_empleado_vencimiento = :id_empleado_vencimiento";
+        $stmt->dpPrepare($query);
+        $stmt->dpBind(':id_empleado_vencimiento', $this->getIdEmpleadoVencimiento());
+        $stmt->dpExecute();
+        return $stmt->dpGetAffect();
+    }
+
+
+    public function checkEmpleado($id_cuadrilla_empleado, $id_cuadrilla, $id_empleado) {
+        $stmt=new sQuery();
+        $query = "select *
+                  from nov_cuadrilla_empleado
+                  where id_cuadrilla = :id_cuadrilla
+                  and id_empleado = :id_empleado
+                  and id_cuadrilla_empleado <> :id_cuadrilla_empleado";
+        $stmt->dpPrepare($query);
+        $stmt->dpBind(':id_cuadrilla', $id_cuadrilla);
+        $stmt->dpBind(':id_empleado', $id_empleado);
+        $stmt->dpBind(':id_cuadrilla_empleado', $id_cuadrilla_empleado);
+        $stmt->dpExecute();
+        return $output = ($stmt->dpGetAffect()==0)? true : false;
+    }
+
+
+
+}
+
+
+
+
+?>
--- view/vehiculos/vehiculosFormContratos.php
+++ view/vehiculos/vehiculosFormContratos.php
@@ -1,3 +1,75 @@
+<script type="text/javascript">
+    $(document).ready(function(){
+
+        var jsonCompetencias = [];
+
+        // Al presionar alguno de los select de puntajes
+        $(':checkbox').on('click', function(e){
+            //Solo guarda en el array los elementos que cambiaron, no es necesario tener los que vienen de la BD.
+            item = {};
+            item.id_empleado_vencimiento = $(this).val();
+            item.id_empleado = $('#id_empleado').val();
+            item.id_vencimiento = $(this).attr('id_vencimiento');
+            item.operation = '';
+
+
+            if(jsonCompetencias[item.id_vencimiento]) { //lo modifica
+                if(this.checked) jsonCompetencias[item.id_vencimiento].operation = 'checked';
+                else jsonCompetencias[item.id_vencimiento].operacion = 'unchecked';
+            }
+            else { //si no existe, lo agrega
+                jsonCompetencias[item.id_vencimiento] = item;
+                if(this.checked) jsonCompetencias[item.id_vencimiento].operation = 'checked';
+                else jsonCompetencias[item.id_vencimiento].operation = 'unchecked';
+            }
+
+        });
+
+
+        //Al guardar los vencimientos
+        $('#myModal').on('click', '#submit',function(){
+            //alert('guardar evaluacion desempeño');
+            //if ($("#eac-form").valid()){
+            var params={};
+            params.action = 'empleados';
+            params.operation = 'saveVencimientos';
+            //params.periodo = $('#periodo').val();
+            //params.cerrado = $('#cerrado').val();
+            //alert(params.id_compania);
+
+            var jsonCompetenciasIx = $.map(jsonCompetencias, function(item){ return item;} );
+            params.vCompetencias = JSON.stringify(jsonCompetenciasIx);
+
+
+            $.post('index.php',params,function(data, status, xhr){
+                //No se usa .fail() porque el resultado viene de una transaccion (try catch) que siempre devuelve 1 o -1
+                //alert(xhr.responseText);
+                if(data >=0){
+                    $("#myModal button").prop("disabled", true); //deshabilito botones
+                    $("#myElem").html('Vencimientos guardados con exito').addClass('alert alert-success').show();
+                    $("#search").trigger("click");
+                    setTimeout(function() { $("#myElem").hide();
+                        //$('#modalEac').modal('hide');
+                        $("#myModal button").prop("disabled", false); //habilito botones
+                    }, 2000);
+
+                }else{
+                    $("#myElem").html('No es posible guardar los vencimientos').addClass('alert alert-danger').show();
+                }
+
+            }, 'json');
+
+            //}
+            return false;
+        });
+
+
+
+
+    });
+
+</script>
+
 <!-- Modal -->
 <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
     <div class="modal-dialog" role="document">
@@ -94,6 +166,74 @@
                     <br/>
                     <div class="alert alert-warning">
                         <i class="fas fa-exclamation-triangle fa-fw"></i> El vehículo no posee seguro vehicular.
+                    </div>
+
+                <?php } ?>
+
+
+
+
+
+                <br/>
+
+                <h4><span class="label label-primary">Vencimientos obligatorios</span></h4>
+
+
+                <?php if(isset($view->vencimientos) && sizeof($view->vencimientos) > 0) {?>
+
+
+
+
+                    <form name ="etapa-form" id="etapa-form" method="POST" action="index.php">
+                        <fieldset>
+
+                            <div class="alert alert-info">
+                                <i class="fas fa-info-circle"></i> Seleccionar los vencimientos requeridos para el empleado.
+                            </div>
+
+                            <input type="hidden" name="id_empleado" id="id_empleado" value="<?php print $view->empleado->getIdEmpleado() ?>">
+
+                            <div class="table-responsive fixedTable">
+
+                                <?php foreach ($view->vencimientos as $v): ?>
+
+                                    <div class="checkbox">
+                                        <label>
+                                            <input type="checkbox" value="<?php echo $v['id_empleado_vencimiento'] ?>" <?php echo ($v['id_empleado_vencimiento'])? 'checked' : ''; ?>
+                                                   id_vencimiento="<?php echo $v['id_vencimiento'] ?>"
+                                                >
+
+                                            <?php echo $v['nombre']; ?>
+                                        </label>
+                                    </div>
+
+                                <?php endforeach; ?>
+
+                            </div>
+
+
+
+                            <br/>
+
+                            <div id="myElem" class="msg" style="display:none"></div>
+
+
+                            <div id="footer-buttons" class="pull-right">
+                                <button class="btn btn-primary" id="submit" name="submit" type="button" <?php echo ( PrivilegedUser::dhasPrivilege('RPE_ABM', array(1)) )? '' : 'disabled' ?> >Guardar</button>
+                                <!--<button class="btn btn-default" id="cancel" name="cancel" type="button">Cancelar</button>-->
+                            </div>
+
+
+                        </fieldset>
+                    </form>
+
+
+
+                <?php }else{ ?>
+
+                    <br/>
+                    <div class="alert alert-warning">
+                        <i class="fas fa-exclamation-triangle fa-fw"></i> No es posible cargar los vencimientos obligatorios del empleado.
                     </div>
 
                 <?php } ?>
