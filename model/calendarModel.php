4b4ddb87e08aa7e4e7217e97ddce5fffe8974ded
dario <dario@innertrip.com.ar>
2020-05-02 20:23:54 -0300

    GSEIP-79 saved

--- model/calendarModel.php
+++ model/calendarModel.php
@@ -88,7 +88,7 @@ join nov_parte_empleado npe on npe.id_parte = np.id_parte
 join empleados em on em.id_empleado = npe.id_empleado
 left join nov_areas na on na.id_area = np.id_area
 left join nov_eventos_c nec on nec.id_evento = np.id_evento
--- where npe.id_empleado in ($empleados)
+where npe.id_empleado in (ifnull($empleados, npe.id_empleado))
 order by np.id_parte asc";
 
         $stmt->dpPrepare($query);
