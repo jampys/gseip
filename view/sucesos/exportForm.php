3d5e1edda09aa998292a47e133b6a8d71da68dd2
dario <dario@innertrip.com.ar>
2020-05-03 17:10:16 -0300

    GSEIP-79 saved

--- view/sucesos/exportForm.php
+++ view/sucesos/exportForm.php
@@ -100,8 +100,7 @@
         
 
         //para exportar a pdf
-        //$('.table-responsive').on("click", ".pdf", function(){
-        $('#myModal').on("click", "#submit1", function(){
+        /*$('#myModal').on("click", "#submit1", function(){
             //alert('Crosstab sucesos');
             //$('#txt-form').validate().resetForm(); //limpiar error input validate
             $('#txt-form').find('input').closest('.form-group').removeClass('has-error');
@@ -119,9 +118,9 @@
                 params.fecha_desde = $("#myModal #fecha_desde").val();
                 params.fecha_hasta = $("#myModal #fecha_hasta").val();
                 params.id_contrato = $("#myModal #id_contrato").val();
-                params.id_user = "<?php echo $_SESSION['id_user']; ?>";
+                params.id_user = "<?php //echo $_SESSION['id_user']; ?>";
                 var strWindowFeatures = "location=yes,height=500,width=800,scrollbars=yes,status=yes";
-                var URL="<?php echo $GLOBALS['ini']['report_url']; ?>frameset?__format=html&__report=gseip_crossTab_sucesos.rptdesign"+
+                var URL="<?php //echo $GLOBALS['ini']['report_url']; ?>frameset?__format=html&__report=gseip_crossTab_sucesos.rptdesign"+
                     "&p_fecha_desde="+params.fecha_desde+
                     "&p_fecha_hasta="+params.fecha_hasta+
                     "&p_id_contrato="+params.id_contrato+
@@ -129,32 +128,11 @@
                     "&p_id_user="+params.id_user;
                 var win = window.open(URL, "_blank");
 
-
-                /*var attr = $('#search_empleado option:selected').attr('id_empleado'); // For some browsers, `attr` is undefined; for others,`attr` is false.  Check for both.
-                 params.id_empleado = (typeof attr !== typeof undefined && attr !== false)? $('#search_empleado option:selected').attr('id_empleado') : '';
-                 var attr = $('#search_empleado option:selected').attr('id_grupo');
-                 params.id_grupo = (typeof attr !== typeof undefined && attr !== false)? $('#search_empleado option:selected').attr('id_grupo') : '';
-                 //params.id_vencimiento = $("#search_vencimiento").val();
-                 params.id_vencimiento = ($("#search_vencimiento").val()!= null)? $("#search_vencimiento").val() : '';
-                 params.id_contrato = $("#search_contrato").val();
-                 params.renovado = $('#search_renovado').prop('checked')? 1 : '';
-                 params.id_user = <?php echo $_SESSION['id_user']; ?>
-                 //var nro_version = Number($('#version').val());
-                 //var lugar_trabajo = $('#lugar_trabajo').val();
-                 //var usuario  = "<?php echo $_SESSION["USER_NOMBRE"].' '.$_SESSION["USER_APELLIDO"]; ?>";
-                 //var id_cia = "<?php echo $_SESSION['ID_CIA']; ?>";
-                 //var strWindowFeatures = "location=yes,height=500,width=800,scrollbars=yes,status=yes, top=200,left=400";
-
-                 //var URL="<?php echo $GLOBALS['ini']['report_url']; ?>frameset?__format=pdf&__report=sci_plan_version.rptdesign&p_periodo="+periodo+"&p_nro_version="+nro_version+"&p_lugar_trabajo="+lugar_trabajo+"&p_usuario="+usuario+"&p_id_cia="+id_cia;
-                 var URL="<?php echo $GLOBALS['ini']['report_url']; ?>frameset?__format=pdf&__report=gseip_vencimientos_p.rptdesign&p_id_empleado="+params.id_empleado+"&p_id_grupo="+params.id_grupo+"&p_id_vencimiento="+params.id_vencimiento+"&p_id_contrato="+params.id_contrato+"&p_renovado="+params.renovado+"&p_id_cia="+params.id_empleado+"&p_id_user="+params.id_user;
-                 //var win = window.open(URL, "_blank", strWindowFeatures);
-                 */
-
             }
 
 
             return false;
-        });
+        });*/
 
 
         //para exportar a txt
@@ -171,20 +149,11 @@
             if ($("#txt-form").valid()){
 
                 params={};
-                //params.id_empleado = $("#search_empleado").val();
-                //params.eventos = ($("#search_evento").val()!= null)? $("#search_evento").val() : '';
-                //params.eventos = ($("#myModal #id_evento").val()!= null)? $("#myModal #id_evento").val() : '';
-                //params.fecha_desde = $("#myModal #fecha_desde").val();
-                //params.fecha_hasta = $("#myModal #fecha_hasta").val();
                 params.id_contrato = $("#myModal #id_contrato").val();
                 params.id_periodo = $("#myModal #id_periodo").val();
                 //location.href="index.php?action=sucesos&operation=txt";
                 location.href="index.php?action=sucesos"+
                                         "&operation=txt" +
-                                        //"&id_empleado="+params.id_empleado+
-                                        //"&eventos="+params.eventos+
-                                        //"&p_fecha_desde="+params.fecha_desde+
-                                        //"&p_fecha_hasta="+params.fecha_hasta+
                                         "&id_contrato="+params.id_contrato+
                                         "&id_periodo="+params.id_periodo;
 
@@ -234,7 +203,7 @@
                     </div>
 
 
-                    <div class="form-group">
+                    <div class="form-group required">
                         <label for="id_periodo" class="control-label">Período de liquidación</label>
                         <select class="form-control selectpicker show-tick" id="id_periodo" name="id_periodo" title="Seleccione un periodo" data-live-search="true" data-size="5">
                             <!-- se completa dinamicamente desde javascript  -->
@@ -242,32 +211,31 @@
                     </div>
 
 
-                    <div class="form-group">
+                    <!--<div class="form-group">
                         <label class="control-label" for="empleado">Fecha desde / hasta</label>
                         <div class="input-group input-daterange">
                             <input class="form-control" type="text" name="fecha_desde" id="fecha_desde" value = "<?php //print $view->contrato->getFechaDesde() ?>" placeholder="DD/MM/AAAA" readonly>
                             <div class="input-group-addon">a</div>
                             <input class="form-control" type="text" name="fecha_hasta" id="fecha_hasta" value = "<?php //print $view->contrato->getFechaHasta() ?>" placeholder="DD/MM/AAAA" readonly>
                         </div>
-                    </div>
+                    </div>-->
 
 
 
-                    <div class="form-group">
+                    <!--<div class="form-group">
                         <label for="id_evento" class="control-label">Eventos</label>
                         <select multiple class="form-control selectpicker show-tick" id="id_evento" name="id_evento" data-selected-text-format="count" data-actions-box="true" data-live-search="true" data-size="5">
-                            <!--<option value="">Seleccione un vencimiento</option>-->
-                            <?php foreach ($view->eventos as $ev){
+                            <?php //foreach ($view->eventos as $ev){
                                 ?>
-                                <option value="<?php echo $ev['id_evento']; ?>" >
-                                    <?php echo $ev['nombre'] ;?>
+                                <option value="<?php //echo $ev['id_evento']; ?>" >
+                                    <?php //echo $ev['nombre'] ;?>
                                 </option>
-                            <?php  } ?>
+                            <?php  //} ?>
                         </select>
-                    </div>
+                    </div>-->
 
 
-                    <div class="alert alert-info" role="alert">
+                    <!--<div class="alert alert-info" role="alert">
                         <div class="row">
                             <div class="col-sm-10">
                                 <span class="glyphicon glyphicon-tags" ></span>&nbsp Exporta sucesos para un rango de fechas y eventos (opcional) indicados.
@@ -276,7 +244,7 @@
                                 <button class="btn btn-primary" id="submit1" name="submit1" type="submit">&nbsp;<i class="far fa-file-pdf fa-lg"></i>&nbsp;</button>
                             </div>
                         </div>
-                    </div>
+                    </div>-->
 
 
                     <div class="alert alert-info" role="alert">
