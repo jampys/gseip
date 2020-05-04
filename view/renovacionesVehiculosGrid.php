fe68c543a64d1e83776960537ab6c4198843f3a5
dario <dario@innertrip.com.ar>
2020-05-04 19:22:24 -0300

    GSEIP-80 saved

--- view/renovacionesVehiculosGrid.php
+++ view/renovacionesVehiculosGrid.php
@@ -6,27 +6,22 @@
         //$('[data-toggle="tooltip"]').tooltip();
 
         $('#example').DataTable({
+            responsive: true,
             /*language: {
              url: 'dataTables/Spanish.json'
              }*/
-
             "fnInitComplete": function () {
-                                $(this).show(); },
-
-
+                                $(this).show();
+            },
             "stateSave": true,
             "order": [[6, "asc"], [7, "asc"], [5, "asc"] ], //6=priority (oculta), 7=renovacion, 5=fecha_vencimiento
-            /*"columnDefs": [
-                { type: 'date-uk', targets: 1 }, //fecha
-                { type: 'date-uk', targets: 4 }, //fecha_emision
-                { type: 'date-uk', targets: 5 } //fecha_vencimiento
-            ]*/
             columnDefs: [
                 {targets: [ 1 ], type: 'date-uk', orderData: [ 1, 6 ]}, //fecha
                 {targets: [ 4 ], type: 'date-uk', orderData: [ 4, 6 ]}, //fecha_emision
                 {targets: [ 5 ], type: 'date-uk', orderData: [ 5, 6 ]}, //fecha_vencimiento
                 {targets: [ 6 ], orderData: [ 6]}, //priority
-                {targets: [ 7 ], orderData: [ 7]} //renovacion
+                {targets: [ 7 ], orderData: [ 7]}, //renovacion
+                { responsivePriority: 1, targets: 8 }
             ]
         });
 
@@ -76,13 +71,9 @@
 
 <div class="col-md-12">
 
+    <!--<div class="table-responsive">-->
 
-
-
-
-    <div class="table-responsive">
-
-        <table id="example" class="table table-striped table-bordered table-condensed" cellspacing="0" width="100%" style="display: none">
+        <table id="example" class="table table-striped table-bordered table-condensed dt-responsive nowrap" cellspacing="0" width="100%" style="display: none">
             <thead>
             <tr>
                 <th>Nro. rnv</th>
@@ -155,7 +146,7 @@
             <a href="index.php?action="><i class="far fa-file-pdf fa-fw fa-2x dp_blue"></i></a>
         </div>
 
-    </div>
+    <!--</div>-->
 
 </div>
 
