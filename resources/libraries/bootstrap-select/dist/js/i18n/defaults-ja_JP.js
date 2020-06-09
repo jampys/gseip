5d50f7d9401bd1b87c9afd0f735c20f4029d18bf
dario <dario@innertrip.com.ar>
2020-06-09 19:19:12 -0300

    HOTFIX-092 saved

--- resources/libraries/bootstrap-select/dist/js/i18n/defaults-ja_JP.js
+++ resources/libraries/bootstrap-select/dist/js/i18n/defaults-ja_JP.js
@@ -1,11 +1,12 @@
 /*!
- * Bootstrap-select v1.12.4 (http://silviomoreto.github.io/bootstrap-select)
+ * Bootstrap-select v1.13.14 (https://developer.snapappointments.com/bootstrap-select)
  *
- * Copyright 2013-2017 bootstrap-select
- * Licensed under MIT (https://github.com/silviomoreto/bootstrap-select/blob/master/LICENSE)
+ * Copyright 2012-2020 SnapAppointments, LLC
+ * Licensed under MIT (https://github.com/snapappointments/bootstrap-select/blob/master/LICENSE)
  */
 
 (function (root, factory) {
+  if (root === undefined && window !== undefined) root = window;
   if (typeof define === 'function' && define.amd) {
     // AMD. Register as an anonymous module unless amdModuleId is set
     define(["jquery"], function (a0) {
@@ -23,15 +24,16 @@
 
 (function ($) {
   $.fn.selectpicker.defaults = {
-    noneSelectedText: '何もが選択した',
-    noneResultsText: '\'{0}\'が結果を返さない',
-    countSelectedText: '{0}/{1}が選択した',
-    maxOptionsText: ['限界は達した({n}{var}最大)', '限界をグループは達した({n}{var}最大)', ['アイテム', 'アイテム']],
-    selectAllText: '全部を選択する',
-    deselectAllText: '何も選択しない',
+    noneSelectedText: '選択されていません',
+    noneResultsText: '\'{0}\'は見つかりません',
+    countSelectedText: '{0}/{1} 選択中',
+    maxOptionsText: ['選択上限数を超えています(最大{n}{var})', 'グループの選択上限数を超えています(最大{n}{var})', ['アイテム', 'アイテム']],
+    selectAllText: '全て選択',
+    deselectAllText: '選択をクリア',
     multipleSeparator: ', '
   };
 })(jQuery);
 
 
 }));
+//# sourceMappingURL=defaults-ja_JP.js.map
