fd7496bbd1aa0d64e0c7ef7c9859e077478e6884
dario <dario@innertrip.com.ar>
2020-06-11 16:12:31 -0300

    HOTFIX-093 saved

--- view/novedades2/1contratoForm.php
+++ view/novedades2/1contratoForm.php
@@ -156,11 +156,11 @@
                 <div class="row">
 
 
-                    <div class="form-group col-md-3">
+                    <div class="form-group col-md-6">
 
-                    </div>
-
-                    <div class="form-group col-md-3">
+                        <div id="myElem" class="msg" style="display:none">
+                            <ul class="alert alert-danger" style="list-style-type: none"><p></p></ul>
+                        </div>
 
                     </div>
 
