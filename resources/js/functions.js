355ac79021185edb6d8d45ecaaefb44a10035274
dario <dario@innertrip.com.ar>
2020-06-14 21:02:53 -0300

    GSEIP-66 saved

--- resources/js/functions.js
+++ resources/js/functions.js
@@ -6,11 +6,22 @@ $(document).ready(function(){ //cuando el html fue cargado iniciar
 
     /* para el plug-in jquery validation: cuando se produce un error, agrega la clase de bootstrap has-error para pintar el borde del input  y el label de rojo */
     $.validator.setDefaults({
-        highlight: function(element) {
+        /*highlight: function(element) {
             $(element).closest('.form-group').addClass('has-error');
         },
         unhighlight: function(element) {
             $(element).closest('.form-group').removeClass('has-error');
+        },*/
+        highlight: function (element) { //https://github.com/jquery-validation/jquery-validation/issues/2150
+            $(element).closest('.form-group').addClass('has-error');
+        },
+        unhighlight: function (element) {
+            if ($(element).hasClass('is-valid-class')) {
+                $(element).closest('.form-group').removeClass('has-error');
+            }
+        },
+        success: function(lebel, element) {
+            $(element).addClass('is-valid-class');
         },
         errorContainer: '#myElem',
         errorLabelContainer: '#myElem ul',
