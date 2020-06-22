ad1885c8761af9ceabff8a55789c12329076e2fd
dario <dario@innertrip.com.ar>
2020-06-22 19:45:36 -0300

    HOTFIX-096 saved

--- view/vehiculos/vehiculosLayout.php
+++ view/vehiculos/vehiculosLayout.php
@@ -106,38 +106,35 @@
 
 
 
-            $(document).on('click', '.delete', function(){ //ok
-                var id = $(this).attr('data-id');
-                $('#confirm').dialog({ //se agregan botones al confirm dialog y se abre
-                    buttons: [
-                        {
-                            text: "Aceptar",
-                            click: function() {
-                                $.fn.borrar(id);
-                            },
-                            class:"btn btn-danger"
-                        },
-                        {
-                            text: "Cancelar",
-                            click: function() {
-                                $(this).dialog("close");
-                            },
-                            class:"btn btn-default"
-                        }
+            var dialog;
+            $(document).on('click', '.delete', function(){
 
-                    ],
-                    open: function() {
-                        $(this).html(confirmMessage('¿Desea eliminar el vehículo?'));
-                    },
-                    close: function() { $("#myElemento").empty().removeClass(); }
-                }).dialog('open');
-                return false;
+                var id = $(this).attr('data-id');
+                dialog = bootbox.dialog({
+                    message: "<p>¿Desea eliminar el vehículo?</p>",
+                    size: 'small',
+                    buttons: {
+                        cancel: {
+                            label: "No"
+                        },
+                        ok: {
+                            label: "Si",
+                            className: 'btn-danger',
+                            callback: function(){
+                                $.fn.borrar(id);
+                                return false; //evita que se cierre automaticamente
+                            }
+                        }
+                    }
+                });
+
+
             });
 
 
-            $.fn.borrar = function(id) { //ok
+
+            $.fn.borrar = function(id) {
                 //alert(id);
-                //preparo los parametros
                 params={};
                 params.id_vehiculo = id;
                 params.action = "vehiculos";
@@ -145,17 +142,17 @@
 
                 $.post('index.php',params,function(data, status, xhr){
                     if(data >=0){
-                        $("#myElemento").html('Vehículo eliminado con exito').addClass('alert alert-success').show();
-                        $('.ui-dialog .btn').attr("disabled", true); //deshabilito botones
-                        setTimeout(function() { $("#myElemento").hide();
-                                                $('#confirm').dialog('close');
-                                                $('#content').load('index.php',{action:"vehiculos", operation: "refreshGrid"});
-                                              }, 2000);
+                        dialog.find('.modal-footer').html('<div class="alert alert-success">Vehículo eliminado con exito</div>');
+                        setTimeout(function() {
+                            dialog.modal('hide');
+                            $('#content').load('index.php',{action:"vehiculos", operation: "refreshGrid"});
+                        }, 2000);
                     }
 
                 }, 'json').fail(function(jqXHR, textStatus, errorThrown ) {
                     //alert('Entro a fail '+jqXHR.responseText);
-                    $("#myElemento").html('No es posible eliminar el vehículo').addClass('alert alert-danger').show();
+                    dialog.find('.modal-footer').html('<div class="alert alert-danger">No es posible eliminar el vehículo</div>');
+
                 });
 
             };
