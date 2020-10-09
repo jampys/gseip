05c53b2978947c74d65be9c358d19196dc02ea39
dario <dario@innertrip.com.ar>
2020-10-09 19:42:00 -0300

    HOTFIX-100 saved

--- view/novedades_partes/export_txtForm.php
+++ view/novedades_partes/export_txtForm.php
@@ -181,13 +181,15 @@
 
                 params={};
                 params.id_contrato = $("#myModal #id_contrato").val();
+                params.first_contrato = params.id_contrato.split(',')[0];
                 params.periodo = $("#myModal #periodo").val();
                 params.id_user = "<?php echo $_SESSION['id_user']; ?>";
+                alert(params.first_contrato);
 
                 //var strWindowFeatures = "location=yes,height=500,width=800,scrollbars=yes,status=yes, top=200,left=400";
                 var strWindowFeatures = "location=yes,height=500,width=800,scrollbars=yes,status=yes";
                 //var URL="<?php echo $GLOBALS['ini']['report_url']; ?>frameset?__report=gseip_crossTab_novedades.rptdesign&p_id_contrato="+params.id_contrato+"&p_fecha_desde="+params.fecha_desde+"&p_fecha_hasta="+params.fecha_hasta+"&p_id_user="+params.id_user;
-                var URL="<?php echo $GLOBALS['ini']['report_url']; ?>frameset?__report=gseip_nov_control_administracion_"+params.id_contrato+".rptdesign&p_id_contrato="+params.id_contrato+
+                var URL="<?php echo $GLOBALS['ini']['report_url']; ?>frameset?__report=gseip_nov_control_administracion_"+params.first_contrato+".rptdesign&p_id_contrato="+params.id_contrato+
                         //"&p_fecha_desde="+params.fecha_desde+
                         //"&p_fecha_hasta="+params.fecha_hasta+
                     "&p_id_periodo="+params.id_periodo+
