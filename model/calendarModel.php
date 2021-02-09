f1fdaf741560960bd8b3eae55879fc8b7bde19b7
dario <dario@innertrip.com.ar>
2021-02-09 19:21:22 -0300

    HOTFIX-108 saved

--- model/calendarModel.php
+++ model/calendarModel.php
@@ -56,7 +56,7 @@ class Calendar
                   join empleados em on su.id_empleado = em.id_empleado
                   join nov_eventos_l ev on su.id_evento = ev.id_evento
                   left join empleado_contrato ec on su.id_empleado = ec.id_empleado and (ec.fecha_hasta is null or ec.fecha_hasta >= sysdate() )
-                  join nov_periodos pe1 on pe1.id_periodo = su.id_periodo1
+                  left join nov_periodos pe1 on pe1.id_periodo = su.id_periodo1
                   left join nov_periodos pe2 on pe2.id_periodo = su.id_periodo2
                   where su.id_empleado in ($empleados)
                   and su.id_evento in ($eventos)
