9a9c3e2801252b8b83f6d180741923186c595111
dario <dario@innertrip.com.ar>
2021-03-29 09:14:45 -0300

    HOTFIX-109 saved

--- view/sucesos/sucesosForm.php
+++ view/sucesos/sucesosForm.php
@@ -611,7 +611,7 @@
             </div>
 
             <div class="modal-footer">
-                <button class="btn btn-primary" id="submit" name="submit" type="submit">Guardar</button>
+                <button class="btn btn-primary" id="submit" name="submit" type="submit" <?php echo ( PrivilegedUser::dhasPrivilege('SUC_ABM', array(1)) && $view->target!='view')? '' : 'disabled' ?> >Guardar</button>
                 <button class="btn btn-default" id="cancel" name="cancel" type="button" data-dismiss="modal">Cancelar</button>
             </div>
 
