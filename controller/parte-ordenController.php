bcb94e090aedf90f8331b7f7694ca6ffe58fc86a
dario <dario@innertrip.com.ar>
2021-04-15 15:22:43 -0300

    HOTFIX-111 saved

--- controller/parte-ordenController.php
+++ controller/parte-ordenController.php
@@ -61,9 +61,9 @@ switch ($operation)
         //$view->label = ($_POST['target']!='view')? 'Editar orden': 'Ver orden';
         $view->orden = new ParteOrden($_POST['id_parte_orden']);
 
-        if($_POST['target'] == 'edit' or $_POST['target'] == 'view' ) $view->label = "Parte diario ".$view->orden->getNroParteDiario();
+        if($_POST['target'] == 'edit' or $_POST['target'] == 'view' ) $view->label = "Nro. parte diario ".$view->orden->getNroParteDiario();
         else if ($_POST['target'] == 'clone') {
-            $view->label = '<span class="label label-warning">CLONAR</span> Parte diario '.$view->orden->getNroParteDiario();
+            $view->label = '<span class="label label-warning">CLONAR</span> Nro. parte diario '.$view->orden->getNroParteDiario();
             $view->orden->setIdParteOrden(null); //pone el id_objetivo en null para al guardar insertar uno nuevo
         }
 
