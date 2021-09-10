9b0645a8043c862269a44486cecd009b85631935
dario <dario@innertrip.com.ar>
2021-09-10 09:33:33 -0300

    HOTFIX-119 saved

--- view/sucesos/generador_21.php
+++ view/sucesos/generador_21.php
@@ -161,7 +161,54 @@ $fila4['analisis_causa_desc'] = nl2br($nc->getAnalisisCausaDesc());*/
         </div>
 
 
+
         <br/>
+
+
+
+                <div style="width:49%; float:left">
+        <span class="titulo">Patrón de Calibración Presión</span>
+        <div class="borde-circular" >
+        <table style="width:100%">
+            <tbody>
+                <tr>
+                    <td style="width: 50%"><span class="subtitulo">Marca</span></td>
+                    <td style="width: 50%">'.$fila['ip_marca'].'</td>
+                </tr>
+                <tr>
+                    <td><span class="subtitulo">Rango de Medición</span></td>
+                    <td>'.$fila['ip_rango'].'</td>
+                </tr>
+                <tr>
+                    <td><span class="subtitulo">N° de Serie</span></td>
+                    <td>'.$fila['ip_nro_serie'].'</td>
+                </tr>
+                <tr>
+                    <td><span class="subtitulo">Fecha de Calibración</span></td>
+                    <td>'.$fila['ip_fecha_calibracion'].'</td>
+                </tr>
+            </tbody>
+        </table>
+        </div>
+
+        </div>
+
+        <div style="width:1%; float:left">&nbsp;</div>
+
+        <div style="width:50%; float:left">
+        <span class="titulo">Patrón de Calibración Caudal</span>
+        <div class="borde-circular" >
+
+        </div>
+
+        </div>
+
+
+        <br/>
+
+
+
+
 
          <div style="float: left; width: 100%">
             <span class="titulo">Tipo de No conformidad</span>
