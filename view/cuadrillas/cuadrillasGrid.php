70529a20461e45af8815a5aeef933ba7382ace87
dario <dario@innertrip.com.ar>
2021-11-10 09:03:12 -0300

    HOTFIX-122 saved

--- view/cuadrillas/cuadrillasGrid.php
+++ view/cuadrillas/cuadrillasGrid.php
@@ -5,11 +5,8 @@
 
         //$('[data-toggle="tooltip"]').tooltip();
 
-        $('#example').DataTable({
+        /*$('#example').DataTable({
             responsive: true,
-            /*language: {
-             url: 'dataTables/Spanish.json'
-             }*/
             "fnInitComplete": function () {
                                 $(this).show();
             },
@@ -18,34 +15,82 @@
             columnDefs: [
                 { responsivePriority: 1, targets: 4 }
             ]
+        });*/
+
+
+        $('#example').DataTable({
+            responsive: true,
+            language: {
+                url: 'resources/libraries/dataTables/Spanish.json'
+            },
+            "fnInitComplete": function () {
+                $(this).show();
+            },
+            'ajax': {
+                "type"   : "POST",
+                "url"    : 'index.php',
+                "data": function ( d ) {
+                    d.startDate = $('#daterange').data('daterangepicker').startDate.format('YYYY-MM-DD'); //drp.startDate.format('YYYY-MM-DD');
+                    d.endDate = $('#daterange').data('daterangepicker').endDate.format('YYYY-MM-DD'); //drp.endDate.format('YYYY-MM-DD');
+                    d.search_responsable_ejecucion = $('#search_responsable_ejecucion').val();
+                    d.action = "nc_no_conformidad";
+                    d.operation = "refreshGrid";
+                },
+                "dataSrc": ""
+            },
+            'columns': [
+                {"data" : "nro_no_conformidad"},
+                {"data" : "fecha_implementacion"},
+                {"data" : "nombre"},
+                {"data" : "tipo"},
+                {"data" : "tipo_accion"},
+                {"data" : "responsable_seguimiento"},
+                {"data" : "estado"},
+                {data: null, defaultContent: '', orderable: false}
+            ],
+            createdRow: function (row, data, dataIndex) {
+                $(row).attr('data-id', data.id_no_conformidad);
+            },
+            "columnDefs": [
+                {
+                    targets: 7,//action buttons
+                    responsivePriority: 3,
+                    render: function (data, type, row, meta) {
+                        let permisoAcciones = '<?php echo ( PrivilegedUser::dhasPrivilege('NC_ABM', array(1)) )? 'acciones' : 'disabled' ?>';
+                        let permisoVerificaciones = '<?php echo ( PrivilegedUser::dhasPrivilege('NC_ABM', array(1)) )? 'verificaciones' : 'disabled' ?>';
+                        let permisoEditar = '<?php echo ( PrivilegedUser::dhasPrivilege('NC_ABM', array(1)) )? 'edit' : 'disabled' ?>';
+                        let permisoEliminar = '<?php echo ( PrivilegedUser::dhasPrivilege('NC_ABM', array(1)) )? 'delete' : 'disabled' ?>';
+                        let link = 'index.php?action=nc_no_conformidad&operation=pdf&id_no_conformidad='+row.id_no_conformidad;
+                        let user_info = row.user.split('@')[0]+' '+row.created_date;
+                        return '<a class="'+permisoAcciones+'" href="#" title="Acciones">'+ //si tiene permiso para ver Acciones
+                            '<i class="fas fa-th-list dp_blue"></i>'+
+                            '</a>&nbsp;&nbsp;'+
+                            '<a class="'+permisoVerificaciones+'" href="#" title="Verificaicones">'+ //si tiene permiso para ver Verificaciones
+                            '<i class="fas fa-th-list dp_blue"></i>'+
+                            '</a>&nbsp;&nbsp;'+
+                            '<a class="view" title="Ver" href="#">'+
+                            '<i class="far fa-eye dp_blue"></i>'+
+                            '</a>&nbsp;&nbsp;'+
+                            '<a class="'+permisoEditar+'" href="#" title="Editar">'+ //si tiene permiso para editar
+                            '<i class="far fa-edit dp_blue"></i>'+
+                            '</a>&nbsp;&nbsp;'+
+                            '<a class="'+permisoEliminar+'" href="#" title="Eliminar">'+ //si tiene permiso para eliminar
+                            '<i class="far fa-trash-alt dp_red"></i>'+
+                            '</a>&nbsp;&nbsp;'+
+                            '<a target="_blank" href="'+link+'" title="Descargar certificado">'+
+                            '<i class="fas fa-download dp_blue"></i>'+
+                            '</a>&nbsp;&nbsp;'+
+                            '<a href="#" title="'+user_info+'" onclick="return false;">'+
+                            '<i class="fa fa-question-circle dp_light_gray"></i>'+
+                            '</a>';
+                    }
+                }
+            ]
+
+
         });
 
 
-
-        $('#content').on("click", ".pdf", function(){
-            alert('Funcionalidad en contrucción');
-            /*params={};
-            var attr = $('#search_empleado option:selected').attr('id_empleado'); // For some browsers, `attr` is undefined; for others,`attr` is false.  Check for both.
-            params.id_empleado = (typeof attr !== typeof undefined && attr !== false)? $('#search_empleado option:selected').attr('id_empleado') : '';
-            var attr = $('#search_empleado option:selected').attr('id_grupo');
-            params.id_grupo = (typeof attr !== typeof undefined && attr !== false)? $('#search_empleado option:selected').attr('id_grupo') : '';
-            //params.id_vencimiento = $("#search_vencimiento").val();
-            params.id_vencimiento = ($("#search_vencimiento").val()!= null)? $("#search_vencimiento").val() : '';
-            params.id_contrato = $("#search_contrato").val();
-            params.renovado = $('#search_renovado').prop('checked')? 1 : '';
-            params.id_user = <?php //echo $_SESSION['id_user']; ?>
-            //var nro_version = Number($('#version').val());
-            //var lugar_trabajo = $('#lugar_trabajo').val();
-            //var usuario  = "<?php //echo $_SESSION["USER_NOMBRE"].' '.$_SESSION["USER_APELLIDO"]; ?>";
-            //var id_cia = "<?php //echo $_SESSION['ID_CIA']; ?>";
-            //var strWindowFeatures = "location=yes,height=500,width=800,scrollbars=yes,status=yes, top=200,left=400";
-            var strWindowFeatures = "location=yes,height=500,width=800,scrollbars=yes,status=yes";
-            //var URL="<?php echo $GLOBALS['ini']['application']['report_url']; ?>frameset?__format=pdf&__report=sci_plan_version.rptdesign&p_periodo="+periodo+"&p_nro_version="+nro_version+"&p_lugar_trabajo="+lugar_trabajo+"&p_usuario="+usuario+"&p_id_cia="+id_cia;
-            var URL="<?php echo $GLOBALS['ini']['application']['report_url']; ?>frameset?__format=pdf&__report=gseip_vencimientos_p.rptdesign&p_id_empleado="+params.id_empleado+"&p_id_grupo="+params.id_grupo+"&p_id_vencimiento="+params.id_vencimiento+"&p_id_contrato="+params.id_contrato+"&p_renovado="+params.renovado+"&p_id_cia="+params.id_empleado+"&p_id_user="+params.id_user;
-            //var win = window.open(URL, "_blank", strWindowFeatures);
-            var win = window.open(URL, "_blank");*/
-            return false;
-        });
 
 
 
@@ -61,12 +106,10 @@
 <div class="col-md-10">
 
 
-    <!--<div class="table-responsive">-->
 
-        <table id="example" class="table table-striped table-bordered table-condensed dt-responsive nowrap" cellspacing="0" width="100%" style="display: none">
+        <table id="example" class="table table-striped table-bordered table-condensed dt-responsive nowrap" cellspacing="0" width="100%">
             <thead>
             <tr>
-                <!--<th>Nro. cuad.</th>-->
                 <th>Nombre</th>
                 <th>Identificador</th>
                 <th>Contrato</th>
@@ -74,56 +117,10 @@
                 <th>Área</th>
                 <th>Activa</th>
                 <th></th>
-
             </tr>
             </thead>
-            <tbody>
-
-            <?php if(isset($view->cuadrillas)) {
-                foreach ($view->cuadrillas as $rp):   ?>
-                    <tr data-id="<?php echo $rp['id_cuadrilla']; ?>">
-                        <!--<td><?php //echo $rp['id_cuadrilla']; ?></td>-->
-                        <td><?php echo $rp['nombre']; ?></td>
-                        <td><?php echo $rp['nombre_corto']; ?></td>
-                        <td><?php echo $rp['contrato']; ?></td>
-                        <td><?php echo $rp['vehiculo']; ?></td>
-                        <td><?php echo $rp['area']; ?></td>
-                        <td class="text-center"><?php echo ($rp['disabled'] != 1)? '<i class="fas fa-check-circle fa-fw dp_green" title="activa"></i>' : '<i class="fas fa-minus-circle fa-fw dp_red" title="desactivada"></i>'; ?></td>
-
-                        <!-- si tiene permiso para agregar empleados -->
-                        <td class="text-center">
-                            <a class="empleados" href="javascript:void(0);">
-                                <i class="far fa-list-alt fa-fw dp_blue" title="empleados"></i>
-                            </a>&nbsp;&nbsp;
-
-                        <!-- si tiene permiso para ver -->
-                            <a class="<?php echo ( PrivilegedUser::dhasPrivilege('CUA_VER', array(1)) )? 'view' : 'disabled' ?>" href="javascript:void(0);">
-                                <span class="glyphicon glyphicon-eye-open dp_blue" title="ver" aria-hidden="true"></span>
-                            </a>&nbsp;&nbsp;
-
-                        <!-- si tiene permiso para editar -->
-                            <a class="<?php echo ( PrivilegedUser::dhasAction('CUA_UPDATE', array(1)) )? 'edit' : 'disabled' ?>" href="javascript:void(0);">
-                                <span class="glyphicon glyphicon-edit dp_blue" title="editar" aria-hidden="true"></span>
-                            </a>&nbsp;&nbsp;
-
-                        <!-- si tiene permiso para eliminar -->
-                            <a class="<?php echo ( PrivilegedUser::dhasAction('CUA_DELETE', array(1)) )? 'delete' : 'disabled' ?>" title="borrar" href="javascript:void(0);">
-                                <span class="glyphicon glyphicon-trash dp_red" aria-hidden="true"></span>
-                            </a>
-                        </td>
-
-                    </tr>
-                <?php endforeach; } ?>
-            </tbody>
         </table>
 
-
-        <!--<br/>
-        <div class="pull-right pdf">
-            <a href="index.php?action="><i class="far fa-file-pdf fa-fw fa-2x"></i></a>
-        </div>-->
-
-    <!--</div>-->
 
 </div>
 
