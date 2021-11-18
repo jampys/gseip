1481c717395d2d323a1b993020db5e08c60ef395
dario <dario@innertrip.com.ar>
2021-11-18 13:45:37 -0300

    HOTFIX-122 saved

--- model/nov_partesModel.php
+++ model/nov_partesModel.php
@@ -155,7 +155,8 @@ class Parte
 
     public static function getPartes($fecha_desde, $fecha_hasta, $id_contrato, $id_periodo, $cuadrilla) { //ok
         $stmt=new sQuery();
-        $query="select pa.id_parte,
+        /*
+         * $query="select pa.id_parte,
                     (select count(*) from nov_parte_orden npox where npox.id_parte = pa.id_parte) as orden_count,
                     (select count(*) from nov_parte_empleado_concepto npecx join nov_parte_empleado npex on npex.id_parte_empleado = npecx.id_parte_empleado where npex.id_parte = pa.id_parte) as concept_count,
                     DATE_FORMAT(pa.created_date,  '%d/%m/%Y') as created_date,
@@ -180,6 +181,33 @@ class Parte
                     and pa.id_periodo =  ifnull(:id_periodo, pa.id_periodo)
                     and pa.cuadrilla =  ifnull(:cuadrilla, pa.cuadrilla)
                     order by pa.fecha_parte asc";
+         */
+        $query="select pa.id_parte,
+                    (select count(*) from nov_parte_orden npox where npox.id_parte = pa.id_parte) as orden_count,
+                    (select count(*) from nov_parte_empleado_concepto npecx join nov_parte_empleado npex on npex.id_parte_empleado = npecx.id_parte_empleado where npex.id_parte = pa.id_parte) as concept_count,
+                    DATE_FORMAT(pa.created_date,  '%d/%m/%Y') as created_date,
+                    DATE_FORMAT(pa.fecha_parte,  '%d/%m/%Y') as fecha_parte,
+                    pa.cuadrilla, pa.id_area, pa.id_vehiculo, pa.id_evento, pa.id_contrato, pa.last_calc_status,
+                    concat(ar.codigo, ' ', ar.nombre) as area,
+                    ve.nro_movil as vehiculo,
+                    concat(nec.codigo, ' ', nec.nombre) as evento,
+                    co.nombre as contrato,
+                    us.user, pa.created_by,
+                    pa.id_periodo, pe.closed_date
+                    from nov_partes pa
+                    left join nov_areas ar on pa.id_area = ar.id_area
+                    left join vto_vehiculos ve on pa.id_vehiculo = ve.id_vehiculo
+                    left join nov_eventos_c nec on pa.id_evento = nec.id_evento
+                    join v_sec_contratos_control co on pa.id_contrato = co.id_contrato
+                    join sec_users us on pa.created_by = us.id_user
+                    join nov_periodos pe on pe.id_periodo = pa.id_periodo
+                    and pa.fecha_parte between if(:fecha_desde is null, pa.fecha_parte, :fecha_desde)
+                    and if(:fecha_hasta is null, pa.fecha_parte, :fecha_hasta)
+                    and pa.id_contrato =  ifnull(:id_contrato, pa.id_contrato)
+                    and pa.id_periodo =  ifnull(:id_periodo, pa.id_periodo)
+                    and pa.cuadrilla =  ifnull(:cuadrilla, pa.cuadrilla)
+                    order by pa.fecha_parte asc
+                    limit 500";
         $stmt->dpPrepare($query);
         $stmt->dpBind(':fecha_desde', $fecha_desde);
         $stmt->dpBind(':fecha_hasta', $fecha_hasta);
