f33a3dcec54494b506ce4f8afa90c0d53bcce15c
dario <dario@innertrip.com.ar>
2021-11-29 09:43:29 -0300

    HOTFIX-125 saved

--- view/novedades_partes/generador_excel.php
+++ view/novedades_partes/generador_excel.php
@@ -11,12 +11,14 @@ $sheet->setCellValue('A1', 'Hello World !');
 
 $writer = new Xlsx($spreadsheet);
 //$writer->save('C:/temp/hello world.xlsx');
+$filename = 'partes.xlsx';
 header("Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
-header('Content-Disposition: attachment;filename="Productos.xlsx"');
+header('Content-Disposition: attachment;filename="'.$filename.'"');
 header('Cache-Control: max-age=0');
 
+ob_end_clean(); //https://github.com/PHPOffice/PhpSpreadsheet/issues/217
 $writer->save('php://output');
-
+exit();
 
 
 ?>
