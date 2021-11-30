c6842557636524967f8150213c3a5c8cca743ba4
dario <dario@innertrip.com.ar>
2021-11-30 15:35:24 -0300

    HOTFIX-123.1 saved

--- templates/libraries.php
+++ templates/libraries.php
@@ -1,5 +1,5 @@
 
-    <title>Gestión SEIP</title>
+    <title>SEIP Gestión</title>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 
