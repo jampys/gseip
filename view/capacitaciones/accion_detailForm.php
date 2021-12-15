7abd6dcc328f0deff49aa932201c7a0e853f9905
dario <dario@innertrip.com.ar>
2021-12-15 10:41:39 -0300

    HOTFIX-124 saved

---
+++ view/capacitaciones/accion_detailForm.php
@@ -0,0 +1,150 @@
+<script type="text/javascript">
+
+
+    $(document).ready(function(){
+
+        //Necesario para que funcione el plug-in bootstrap-select
+        $('.selectpicker').selectpicker({
+            //propiedades del selectpicker
+
+        }).change(function(){
+            $(this).valid(); //Este trick de change ... valida hay que hacerlo para que despues de seleccionar un valor
+                             // elimine el mensaje de requerido de jquery validation
+        });
+
+
+        moment.locale('es');
+        $('#fecha_implementacion').daterangepicker({
+            singleDatePicker: true,
+            showDropdowns: true,
+            autoApply: true,
+            autoUpdateInput: false,
+            parentEl: '#myModal',
+            "locale": {
+                "format": "DD/MM/YYYY"
+            }
+        }).on("apply.daterangepicker", function (e, picker) {
+            picker.element.val(picker.startDate.format(picker.locale.format));
+            picker.element.valid();
+        });
+
+
+        $('#myModal').modal({
+            backdrop: 'static',
+            keyboard: false
+        });
+
+
+        //cancel de formulario de etapa
+        $('#etapa-form #cancel').on('click', function(){
+            $('#etapa-form').hide();
+        });
+
+
+        $('#etapa-form').validate({ //ok
+            rules: {
+                /*codigo: {
+                        required: true,
+                        digits: true,
+                        maxlength: 6
+                },*/
+                accion: {
+                    required: true,
+                    maxlength: 200
+                },
+                fecha_implementacion: {required: true},
+                id_responsable_ejecucion: {required: true}
+            },
+            messages:{
+                /*codigo: {
+                    required: "Ingrese el código",
+                    digits: "Ingrese solo números",
+                    maxlength: "Máximo 6 dígitos"
+                }, */
+                accion: {
+                    required: "Ingrese una descripción de la acción",
+                    maxlength: "Máximo 200 caracteres"
+                },
+                etapa: "Seleccione una etapa",
+                fecha_implementacion: "Seleccione la fecha de implementación",
+                id_responsable_ejecucion: "Seleccione el responsable de ejecución"
+            }
+
+        });
+
+
+
+    });
+
+</script>
+
+
+
+<form name ="etapa-form" id="etapa-form" method="POST" action="index.php">
+    <fieldset>
+
+    <div class="alert alert-info">
+        <strong><?php echo $view->label ?></strong>
+    </div>
+
+    <input type="hidden" name="id_accion" id="id_accion" value="<?php print $view->accion->getIdAccion() ?>">
+    <input type="hidden" name="id_no_conformidad" id="id_no_conformidad" value="<?php print $view->accion->getIdNoConformidad() ?>">
+
+        <div class="form-group required">
+            <label class="control-label" for="accion">Acción</label>
+            <textarea class="form-control" name="accion" id="accion" placeholder="Descripción de la acción" rows="4"><?php print $view->accion->getAccion(); ?></textarea>
+        </div>
+
+    <div class="form-group required">
+        <label class="control-label" for="fecha_implementacion">Fecha implementación</label>
+        <div class="inner-addon right-addon">
+            <input class="form-control" type="text" name="fecha_implementacion" id="fecha_implementacion" value = "<?php print $view->accion->getFechaImplementacion() ?>" placeholder="DD/MM/AAAA" readonly>
+            <i class="glyphicon glyphicon-calendar"></i>
+        </div>
+    </div>
+
+
+        <div class="form-group required">
+            <label for="id_responsable_ejecucion" class="control-label">Responsable ejecución</label>
+            <select id="id_responsable_ejecucion" name="id_responsable_ejecucion" class="form-control selectpicker show-tick" data-live-search="true" data-size="5" title="Seleccione un responsable ejecución">
+                <?php foreach ($view->empleados as $em){
+                    ?>
+                    <option value="<?php echo $em['id_empleado']; ?>"
+                        <?php echo ($em['id_empleado'] == $view->accion->getIdResponsableEjecucion())? 'selected' :'' ?>
+                        >
+                        <?php echo $em['apellido'].' '.$em['nombre']; ?>
+                    </option>
+                <?php  } ?>
+            </select>
+        </div>
+
+
+
+
+    <div id="myElem" class="msg" style="display:none">
+        <ul class="alert alert-danger" style="list-style-type: none"><p></p></ul>
+    </div>
+
+
+
+    <div id="footer-buttons" class="pull-right">
+        <button class="btn btn-primary" id="submit" name="submit" type="submit"  <?php echo ( PrivilegedUser::dhasPrivilege('NC_ABM', array(1)) && $view->target!='view')? '' : 'disabled' ?>  >Guardar</button>
+        <button class="btn btn-default" id="cancel" name="cancel" type="button">Cancelar</button>
+    </div>
+
+
+    </fieldset>
+</form>
+
+
+
+
+
+
+
+
+
+
+
+
+
--- view/capacitaciones/accionesGrid.php
+++ view/capacitaciones/accionesGrid.php
@@ -1,18 +1,13 @@
-<style>
-    .dataTables_scrollHeadInner{  width:100% !important; }
-    .dataTables_scrollHeadInner table{  width:100% !important; }
-</style>
-
 <script type="text/javascript">
 
 
     $(document).ready(function(){
 
-        var t = $('#table-tareas').DataTable({
+        var t = $('#table-acciones').DataTable({
             responsive: true,
             language: {
                 //url: 'resources/libraries/dataTables/Spanish.json',
-                emptyTable: 'El objetivo no tiene actividades registradas'
+                emptyTable: 'La No conformidad no tiene acciones registradas'
             },
             sDom: '<"top">rt<"bottom"><"clear">', // http://legacy.datatables.net/usage/options#sDom
             bPaginate: false,
@@ -20,49 +15,43 @@
             scrollY:        150,
             scrollCollapse: true,
             scroller:       true,
-            autoWidth: false,
             order: [[0, "asc"]], // 0=fecha_implementacion
             'ajax': {
                 "type"   : "POST",
                 "url"    : 'index.php',
                 "data": function ( d ) {
-                    d.action = "obj_tareas";
+                    d.action = "nc_acciones";
                     d.operation = "refreshGrid";
-                    d.id_objetivo = $('#myModal #id_objetivo').val(); //$('#etapas_left_side #add').attr('id_objetivo');
+                    d.id_no_conformidad = $('#etapas_left_side #add').attr('id_no_conformidad');
                 },
                 "dataSrc": ""
             },
             'columns': [
-                {"data" : "nombre"},
-                {"data" : "fecha_inicio"},
-                {"data" : "fecha_fin"},
+                {"data" : "fecha_implementacion"},
+                {"data" : "accion"},
                 {data: null, defaultContent: '', orderable: false}
             ],
             createdRow: function (row, data, dataIndex) {
-                $(row).attr('data-id', data.id_tarea);
+                $(row).attr('data-id', data.id_accion);
             },
             "columnDefs": [
+                //{ targets: 0, responsivePriority: 2 },
+                {targets: 0, type: 'date-uk'}, //fecha_implementacion
                 {
-                    targets: 0, //nombre
-                    width: '50%',
+                    targets: 1, //accion
                     render: function(data, type, row) {
-                        return $.fn.dataTable.render.ellipsis(50)(data, type, row);
+                        return $.fn.dataTable.render.ellipsis(125)(data, type, row);
                     }
                 },
-                {targets: 1, type: 'date-uk'}, //fecha_inicio
-                {targets: 2, type: 'date-uk'}, //fecha_fin
                 {
-                    targets: 3,//action buttons
-                    width: '25%',
+                    targets: 2,//action buttons
+                    width: '20%',
                     responsivePriority: 1,
                     render: function (data, type, row, meta) {
-                        let permisoEditar = '<?php echo ( PrivilegedUser::dhasPrivilege('OBJ_ABM', array(1)) && !$view->params['cerrado'] )? 'edit' : 'disabled' ?>';
-                        let permisoEliminar = '<?php echo ( PrivilegedUser::dhasPrivilege('OBJ_ABM', array(1)) && !$view->params['cerrado'] )? 'delete' : 'disabled' ?>';
-                        let user_info = 'próximamente información'; //row.user.split('@')[0]+' '+row.created_date;
-                        return '<a class="avance" title="Avances" href="#">'+
-                                    '<i class="fas fa-forward fa-fw dp_blue"></i>'+
-                                '</a>&nbsp;&nbsp;'+
-                                '<a class="view" title="Ver" href="#">'+
+                        let permisoEditar = '<?php echo ( PrivilegedUser::dhasPrivilege('NC_ABM', array(1)) )? 'edit' : 'disabled' ?>';
+                        let permisoEliminar = '<?php echo ( PrivilegedUser::dhasPrivilege('NC_ABM', array(1)) )? 'delete' : 'disabled' ?>';
+                        let user_info = row.user.split('@')[0]+' '+row.created_date;
+                        return '<a class="view" title="Ver" href="#">'+
                                     '<i class="far fa-eye dp_blue"></i>'+
                                 '</a>&nbsp;&nbsp;'+
                                 '<a class="'+permisoEditar+'" href="#" title="Editar">'+ //si tiene permiso para editar
@@ -80,7 +69,7 @@
         });
 
         setTimeout(function () {
-            t.columns.adjust();
+                    t.columns.adjust();
         },150);
 
 
@@ -92,25 +81,16 @@
 
 
 
-    <div>
-
-        <table id="table-tareas" class="table table-condensed table-hover dt-responsive" width="100%">
-            <thead>
-            <tr>
-                <th>Actividad</th>
-                <th>F. Inicio</th>
-                <th>F. Fin</th>
-                <th></th>
-            </tr>
-            </thead>
-        </table>
-
-
+    
+    <div id="empleados-table">
+            <table id="table-acciones" class="table table-condensed table-hover dt-responsive" width="100%">
+                <thead>
+                <tr>
+                    <th>F. impl.</th>
+                    <th>Acción</th>
+                    <th></th>
+                </tr>
+                </thead>
+            </table>
     </div>
-
-
-
-
-
-
 
