9a9f3e392abf742ce238025cb9487591d7e2242c
dario <dario@innertrip.com.ar>
2022-02-22 20:40:29 -0300

    HOTFIX-127 saved

--- view/capacitaciones/generador.php
+++ view/capacitaciones/generador.php
@@ -194,7 +194,7 @@ $html1 = '<div style="float: left; width: 100%">
             <tbody>';
 
 foreach ($fila5 as $x) {
-    $asistio = ($x["asistio"] == 1)? '<i class="fa-solid fa-check"></i>' : '';
+    $asistio = ($x["asistio"] == 1)? 'SI' : 'NO';
     $html1.='<tr><td>'.$x["empleado"].'</td>><td>'.$x["contrato"].'</td><td>'.$x["edicion"].'</td><td>'.$asistio.'</td></tr>';
 }
 
