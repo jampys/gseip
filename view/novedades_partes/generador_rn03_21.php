84865d4abde860f6f902f251f443dfb12cc01e97
dario <dario@innertrip.com.ar>
2022-05-11 19:13:06 -0300

    HOTFIX-129.1 saved

--- view/novedades_partes/generador_rn03_21.php
+++ view/novedades_partes/generador_rn03_21.php
@@ -41,16 +41,24 @@ $cabecera = ["Empleado",
             "Viandas Extra",
             "Enfermedad",
             "Accidente",
-            "DRT",
-            "DHT",
-            "DHNT",
-            "DCNT",
-            "DHDD",
-            "DH"
+            "Injustificadas",
+            "Permiso días trámite",
+            "Permiso gremial",
+            "Resto Permisos",
+            "Vacaciones",
+            "Suspención",
+            "Mayor Función",
+            "DD Háb. período",
+            "DRT período",
+            "DHT período",
+            "DHNT período",
+            "DCNT período",
+            "DH calendario",
+            "Observaciones"
         ];
 $sheet->fromArray($cabecera, null, 'A8');
-$spreadsheet->getActiveSheet()->getStyle('A8:R8')->getFont()->setBold(true);
-$spreadsheet->getActiveSheet()->getStyle('A8:R8')->getFill()->setFillType(\PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID)->getStartColor()->setARGB('E6E6E6');
+$spreadsheet->getActiveSheet()->getStyle('A8:Z8')->getFont()->setBold(true);
+$spreadsheet->getActiveSheet()->getStyle('A8:Z8')->getFill()->setFillType(\PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID)->getStartColor()->setARGB('E6E6E6');
 
 //cuerpo -----------------------------------------------------------------
 $fila = 9;
@@ -70,12 +78,20 @@ foreach ($view->partes as $p):
     $sheet->setCellValueByColumnAndRow(10, $fila, $p['viandas_extra']);
     $sheet->setCellValueByColumnAndRow(11, $fila, $p['enfermedad']);
     $sheet->setCellValueByColumnAndRow(12, $fila, $p['accidente']);
-    $sheet->setCellValueByColumnAndRow(13, $fila, $p['DRT']);
-    $sheet->setCellValueByColumnAndRow(14, $fila, $p['DHT']);
-    $sheet->setCellValueByColumnAndRow(15, $fila, $p['DHNT']);
-    $sheet->setCellValueByColumnAndRow(16, $fila, $p['DCNT']);
-    $sheet->setCellValueByColumnAndRow(17, $fila, $p['DHDD']);
-    $sheet->setCellValueByColumnAndRow(18, $fila, $p['DH']);
+    $sheet->setCellValueByColumnAndRow(13, $fila, $p['injustificadas']);
+    $sheet->setCellValueByColumnAndRow(14, $fila, $p['permiso_dias_tramite']);
+    $sheet->setCellValueByColumnAndRow(15, $fila, $p['permiso_gremial']);
+    $sheet->setCellValueByColumnAndRow(16, $fila, $p['resto_permisos']);
+    $sheet->setCellValueByColumnAndRow(17, $fila, $p['vacaciones']);
+    $sheet->setCellValueByColumnAndRow(18, $fila, $p['suspenciones']);
+    $sheet->setCellValueByColumnAndRow(19, $fila, $p['mayor_funcion']);
+    $sheet->setCellValueByColumnAndRow(20, $fila, $p['DHDD']);
+    $sheet->setCellValueByColumnAndRow(21, $fila, $p['DRT']);
+    $sheet->setCellValueByColumnAndRow(22, $fila, $p['DHT']);
+    $sheet->setCellValueByColumnAndRow(23, $fila, $p['DHNT']);
+    $sheet->setCellValueByColumnAndRow(24, $fila, $p['DCNT']);
+    $sheet->setCellValueByColumnAndRow(25, $fila, $p['DH']);
+    $sheet->setCellValueByColumnAndRow(26, $fila, $p['mayor_funcion_m']);
 
     $fila++;
 endforeach;
@@ -87,23 +103,32 @@ endforeach;
 }*/
 
 $spreadsheet->getActiveSheet()->getColumnDimension('A')->setWidth(30);
-$spreadsheet->getActiveSheet()->getColumnDimension('B')->setWidth(8);
-$spreadsheet->getActiveSheet()->getColumnDimension('C')->setWidth(8);
-$spreadsheet->getActiveSheet()->getColumnDimension('D')->setWidth(8);
-$spreadsheet->getActiveSheet()->getColumnDimension('E')->setWidth(8);
-$spreadsheet->getActiveSheet()->getColumnDimension('F')->setWidth(8);
-$spreadsheet->getActiveSheet()->getColumnDimension('G')->setWidth(8);
-$spreadsheet->getActiveSheet()->getColumnDimension('H')->setWidth(8);
-$spreadsheet->getActiveSheet()->getColumnDimension('I')->setWidth(8);
-$spreadsheet->getActiveSheet()->getColumnDimension('J')->setWidth(8);
-$spreadsheet->getActiveSheet()->getColumnDimension('K')->setWidth(8);
-$spreadsheet->getActiveSheet()->getColumnDimension('L')->setWidth(8);
-$spreadsheet->getActiveSheet()->getColumnDimension('M')->setWidth(8);
-$spreadsheet->getActiveSheet()->getColumnDimension('N')->setWidth(8);
-$spreadsheet->getActiveSheet()->getColumnDimension('O')->setWidth(8);
-$spreadsheet->getActiveSheet()->getColumnDimension('P')->setWidth(8);
-$spreadsheet->getActiveSheet()->getColumnDimension('Q')->setWidth(8);
-$spreadsheet->getActiveSheet()->getColumnDimension('R')->setWidth(8);
+$spreadsheet->getActiveSheet()->getColumnDimension('B')->setWidth(7);
+$spreadsheet->getActiveSheet()->getColumnDimension('C')->setWidth(7);
+$spreadsheet->getActiveSheet()->getColumnDimension('D')->setWidth(7);
+$spreadsheet->getActiveSheet()->getColumnDimension('E')->setWidth(7);
+$spreadsheet->getActiveSheet()->getColumnDimension('F')->setWidth(7);
+$spreadsheet->getActiveSheet()->getColumnDimension('G')->setWidth(7);
+$spreadsheet->getActiveSheet()->getColumnDimension('H')->setWidth(7);
+$spreadsheet->getActiveSheet()->getColumnDimension('I')->setWidth(7);
+$spreadsheet->getActiveSheet()->getColumnDimension('J')->setWidth(7);
+$spreadsheet->getActiveSheet()->getColumnDimension('K')->setWidth(7);
+$spreadsheet->getActiveSheet()->getColumnDimension('L')->setWidth(7);
+$spreadsheet->getActiveSheet()->getColumnDimension('M')->setWidth(7);
+$spreadsheet->getActiveSheet()->getColumnDimension('N')->setWidth(7);
+$spreadsheet->getActiveSheet()->getColumnDimension('O')->setWidth(7);
+$spreadsheet->getActiveSheet()->getColumnDimension('P')->setWidth(7);
+$spreadsheet->getActiveSheet()->getColumnDimension('Q')->setWidth(7);
+$spreadsheet->getActiveSheet()->getColumnDimension('R')->setWidth(7);
+$spreadsheet->getActiveSheet()->getColumnDimension('S')->setWidth(7);
+$spreadsheet->getActiveSheet()->getColumnDimension('T')->setWidth(7);
+$spreadsheet->getActiveSheet()->getColumnDimension('U')->setWidth(7);
+$spreadsheet->getActiveSheet()->getColumnDimension('V')->setWidth(7);
+$spreadsheet->getActiveSheet()->getColumnDimension('W')->setWidth(7);
+$spreadsheet->getActiveSheet()->getColumnDimension('X')->setWidth(7);
+$spreadsheet->getActiveSheet()->getColumnDimension('Y')->setWidth(7);
+$spreadsheet->getActiveSheet()->getColumnDimension('Z')->setWidth(30);
+
 
 $spreadsheet->getActiveSheet()->getStyle('B8')->getAlignment()->setTextRotation(90);
 $spreadsheet->getActiveSheet()->getStyle('C8')->getAlignment()->setTextRotation(90);
@@ -116,6 +141,19 @@ $spreadsheet->getActiveSheet()->getStyle('I8')->getAlignment()->setTextRotation(
 $spreadsheet->getActiveSheet()->getStyle('J8')->getAlignment()->setTextRotation(90);
 $spreadsheet->getActiveSheet()->getStyle('K8')->getAlignment()->setTextRotation(90);
 $spreadsheet->getActiveSheet()->getStyle('L8')->getAlignment()->setTextRotation(90);
+$spreadsheet->getActiveSheet()->getStyle('M8')->getAlignment()->setTextRotation(90);
+$spreadsheet->getActiveSheet()->getStyle('N8')->getAlignment()->setTextRotation(90);
+$spreadsheet->getActiveSheet()->getStyle('O8')->getAlignment()->setTextRotation(90);
+$spreadsheet->getActiveSheet()->getStyle('P8')->getAlignment()->setTextRotation(90);
+$spreadsheet->getActiveSheet()->getStyle('Q8')->getAlignment()->setTextRotation(90);
+$spreadsheet->getActiveSheet()->getStyle('R8')->getAlignment()->setTextRotation(90);
+$spreadsheet->getActiveSheet()->getStyle('S8')->getAlignment()->setTextRotation(90);
+$spreadsheet->getActiveSheet()->getStyle('T8')->getAlignment()->setTextRotation(90);
+$spreadsheet->getActiveSheet()->getStyle('U8')->getAlignment()->setTextRotation(90);
+$spreadsheet->getActiveSheet()->getStyle('V8')->getAlignment()->setTextRotation(90);
+$spreadsheet->getActiveSheet()->getStyle('W8')->getAlignment()->setTextRotation(90);
+$spreadsheet->getActiveSheet()->getStyle('X8')->getAlignment()->setTextRotation(90);
+$spreadsheet->getActiveSheet()->getStyle('Y8')->getAlignment()->setTextRotation(90);
 
 
 //-----------------------------------------------------------------
