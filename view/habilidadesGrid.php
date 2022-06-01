03614d686086e2fb612726103f06eecfaa9b2acf
dario <dario@innertrip.com.ar>
2022-05-31 21:17:24 -0300

    HOTFIX-129.2 saved

--- view/habilidadesGrid.php
+++ view/habilidadesGrid.php
@@ -54,7 +54,7 @@
                     <td><?php echo $habilidad['nombre'];?></td>
                     <td class="text-center">
                         <a class="view" title="ver" href="javascript:void(0);" data-id="<?php echo $habilidad['id_habilidad'];?>"><i class="far fa-sticky-note dp_blue"></i></a>&nbsp;&nbsp;
-                        <a class="<?php echo (PrivilegedUser::dhasAction('HAB_UPDATE', array(1)))? 'edit' : 'disabled'; ?>" title="editar" href="javascript:void(0);" data-id="<?php echo $habilidad['id_habilidad'];?>"><i class="fas fa-edit dp_blue"></i></a>&nbsp;&nbsp;
+                        <a class="<?php echo (PrivilegedUser::dhasAction('HAB_UPDATE', array(1)))? 'edit' : 'disabled'; ?>" title="editar" href="javascript:void(0);" data-id="<?php echo $habilidad['id_habilidad'];?>"><i class="far fa-edit dp_blue"></i></a>&nbsp;&nbsp;
                         <a class="<?php echo (PrivilegedUser::dhasAction('HAB_DELETE', array(1)))? 'delete' : 'disabled'; ?>" title="borrar" href="javascript:void(0);" data-id="<?php echo $habilidad['id_habilidad'];?>"><i class="far fa-trash-alt dp_red"></i></a>
                     </td>
                 </tr>
