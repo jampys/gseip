c46fc6e4138a537889422692147a0d6247cf4daa
dario <dario@innertrip.com.ar>
2022-06-07 20:16:22 -0300

    HOTFIX-129.3 saved

--- email/password.php
+++ email/password.php
@@ -1,6 +1,19 @@
 <html>
 <head>
 
+
+
+    <style type="text/css">
+        @font-face {
+            font-family: Calibri ;
+            src: url(../resources/fonts/Calibri-Regular.ttf);
+        }
+
+        p{
+            font-family: Calibri;
+        }
+    </style>
+
 </head>
 <body>
 
@@ -8,9 +21,9 @@
         <img style="padding: 10px; width: 60px; height: 20px" src="cid:logo_2u">
     </div>
 
-    <div id="contenido" style="padding: 10px; font-family: 'Roboto', sans-serif">
+    <div id="contenido" style="padding: 10px;">
 
-        <p>Hola <?php //echo $emp[0]['APELLIDO'] ?> <?php //echo $emp[0]['NOMBRE'] ?>,</p>
+        <p>Hola<?php //echo $emp[0]['APELLIDO'] ?><?php //echo $emp[0]['NOMBRE'] ?>,</p>
         <p>Hemos recibido una solicitud para restablecer su contraseña.</p>
 
         <p>Codigo de verificación: <b><?php echo $code; ?></b></p>
