7e284c7627689929508f9f2e28693bda363b5d26
dario <dario@innertrip.com.ar>
2022-06-23 09:06:43 -0300

    HOTFIX-130 saved

--- controller/nov_reportesController.php
+++ controller/nov_reportesController.php
@@ -135,6 +135,37 @@ switch ($operation)
         break;
 
 
+    case 'reporte_rn08':
+        $view->disableLayout=true;
+        $id_contrato = ($_GET['id_contrato'])? $_GET['id_contrato'] : null;
+        $id_periodo = ($_GET['id_periodo'])? $_GET['id_periodo'] : null;
+        $id_empleado = ($_GET['id_empleado'])? $_GET['id_empleado'] : null;
+        $id_concepto = ($_GET['id_concepto'])? $_GET['id_concepto'] : null; //viene el id_concepto_convenio_contrato
+        $view->partes = $rta = ReporteNovedades::getReporteRn6($id_contrato, $id_periodo, $id_empleado, $id_concepto);
+
+        $encabezado = array();
+        $encabezado['obj_contrato'] = new Contrato($_GET['id_contrato']);
+        $encabezado['contrato'] = ($encabezado['obj_contrato']->getIdContrato() > 0)? $encabezado['obj_contrato']->getNroContrato().' '.$encabezado['obj_contrato']->getNombre() : 'Todos';
+        $encabezado['id_compania'] = $encabezado['obj_contrato']->getIdCompania();
+        $encabezado['obj_cliente'] = new Compania($encabezado['id_compania']);
+        $encabezado['cliente'] = ($encabezado['obj_cliente']->getIdCompania() > 0)? $encabezado['obj_cliente']->getRazonSocial() : 'Todos';
+        $encabezado['obj_periodo'] = new NovPeriodo($_GET['id_periodo']);
+        $encabezado['periodo'] = $encabezado['obj_periodo']->getFechaDesde().' - '.$encabezado['obj_periodo']->getFechaHasta();
+
+        $encabezado['obj_empleado'] = new Empleado($_GET['id_empleado']);
+        $encabezado['empleado'] = ($encabezado['obj_empleado']->getIdEmpleado() > 0)? $encabezado['obj_empleado']->getLegajo().' '.$encabezado['obj_empleado']->getApellido().' '.$encabezado['obj_empleado']->getNombre() : 'Todos';
+
+        $encabezado['obj_concepto_convenio_contrato'] = new ConceptoConvenioContrato($_GET['id_concepto']);
+        $encabezado['obj_concepto'] = new Concepto($encabezado['obj_concepto_convenio_contrato']->getIdConcepto());
+        $encabezado['concepto'] = ($encabezado['obj_concepto_convenio_contrato']->getIdConceptoConvenioContrato() > 0)? $encabezado['obj_concepto']->getNombre().' ('.$encabezado['obj_concepto_convenio_contrato']->getCodigo().')' : 'Todos';
+
+
+        $encabezado['fecha_emision'] = date('d/m/Y H:i');
+
+        $view->contentTemplate="view/novedades_partes/generador_rn06.php";
+        break;
+
+
     default :
         $view->areas = NovArea::getAreas(); //carga el combo para filtrar Areas
         $view->contratos = Contrato::getContratosControl(); //carga el combo para filtrar contratos
