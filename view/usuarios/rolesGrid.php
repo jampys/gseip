a751852d8bc0630878c8baa4d7327dc410f3784e
dario <dario@innertrip.com.ar>
2022-06-23 18:34:14 -0300

    HOTFIX-130 saved

--- view/usuarios/rolesGrid.php
+++ view/usuarios/rolesGrid.php
@@ -3,7 +3,7 @@
 
     $(document).ready(function(){
 
-        var t = $('#table-vehiculos').DataTable({
+        /*var t = $('#table-vehiculos').DataTable({
             responsive: true,
             sDom: '<"top"f>rt<"bottom"><"clear">', // http://legacy.datatables.net/usage/options#sDom
             bPaginate: false,
@@ -14,12 +14,112 @@
             columnDefs: [
                 { responsivePriority: 1, targets: 3 }
             ]
+        });*/
+
+        var t = $('#table-empleados').DataTable({
+            responsive: true,
+            language: {
+                //url: 'resources/libraries/dataTables/Spanish.json',
+                search: '',
+                searchPlaceholder: "Buscar empleado",
+                emptyTable: 'La edición no tiene empleados registrados',
+                "sInfo":           "Mostrando _TOTAL_ registros",
+                "sInfoEmpty":      "Mostrando 0 registros",
+                "sInfoFiltered":   ""
+            },
+            sDom:   "<'row'<'col-sm-2'B><'col-sm-4'i><'col-sm-6'f>>" +
+            "<'row'<'col-sm-12'tr>>" +
+            "<'row'<'col-sm-12'>>",
+            buttons: [
+                {
+                    text: '<i class="fas fa-plus fa-fw dp_green"></i>',
+                    titleAttr: 'Agregar participante',
+                    attr:  {
+                        id: 'add', //https://datatables.net/reference/option/buttons.buttons.attr
+                        disabled: function(){
+                            let permisoNuevo = '<?php echo (PrivilegedUser::dhasPrivilege('PTN_ABM', array(1)) )? 'false' : 'true' ?>';
+                            return (permisoNuevo == 'false')? false : true;
+                        }
+                    },
+                    action: function ( e, dt, node, config ) {
+                        //https://datatables.net/reference/option/buttons.buttons.action
+                        //usa el evento que esta en nPostulacionesForm.php
+                    }
+                }
+            ],
+            bPaginate: false,
+            //deferRender:    true,
+            scrollY:        150,
+            scrollCollapse: true,
+            scroller:       true,
+            order: [[0, "asc"]], // 0=fecha_implementacion
+            'ajax': {
+                "type"   : "POST",
+                "url"    : 'index.php',
+                "data": function ( d ) {
+                    d.action = "cap_empleados";
+                    d.operation = "refreshGrid";
+                    d.id_capacitacion = $('#etapas_left_side').attr('id_capacitacion');
+                    d.id_edicion = $('#etapas_left_side').attr('id_edicion');
+                    d.id_contrato = ($("#id_contrato").val()!= null)? $("#id_contrato").val() : '';
+                    d.startDate = $('#daterange').data('daterangepicker').startDate.format('YYYY-MM-DD'); //drp.startDate.format('YYYY-MM-DD');
+                    d.endDate = $('#daterange').data('daterangepicker').endDate.format('YYYY-MM-DD'); //drp.endDate.format('YYYY-MM-DD');
+                },
+                "dataSrc": ""
+            },
+            'columns': [
+                {"data" : "empleado"},
+                {"data" : "contrato"},
+                {"data" : "fecha_edicion"},
+                {data: null, defaultContent: '', orderable: false}
+            ],
+            createdRow: function (row, data, dataIndex) {
+                $(row).attr('data-id', data.id_capacitacion_empleado);
+            },
+            "columnDefs": [
+                {
+                    targets: 0, //empleado
+                    render: function(data, type, row) {
+                        return $.fn.dataTable.render.ellipsis(23)(data, type, row);
+                    }
+                },
+                {
+                    targets: 1, //contrato
+                    render: function(data, type, row) {
+                        return $.fn.dataTable.render.ellipsis(23)(data, type, row);
+                    }
+                },
+                {
+                    targets: 3,//action buttons
+                    width: '18%',
+                    responsivePriority: 1,
+                    render: function (data, type, row, meta) {
+                        let permisoEditar = '<?php echo ( PrivilegedUser::dhasPrivilege('NC_ABM', array(1)) )? 'edit' : 'disabled' ?>';
+                        let permisoEliminar = '<?php echo ( PrivilegedUser::dhasPrivilege('NC_ABM', array(1)) )? 'delete' : 'disabled' ?>';
+                        let user_info = ''; //row.user.split('@')[0]+' '+row.created_date;
+                        return '<a class="view" title="Ver" href="#">'+
+                                    '<i class="far fa-sticky-note dp_blue"></i>'+
+                                '</a>&nbsp;&nbsp;'+
+                                '<a class="'+permisoEditar+'" href="#" title="Editar">'+ //si tiene permiso para editar
+                                    '<i class="far fa-edit dp_blue"></i>'+
+                                '</a>&nbsp;&nbsp;'+
+                                '<a class="'+permisoEliminar+'" href="#" title="Eliminar">'+ //si tiene permiso para eliminar
+                                    '<i class="far fa-trash-alt dp_red"></i>'+
+                                '</a>&nbsp;&nbsp;'+
+                                '<a href="#" title="'+user_info+'" onclick="return false;">'+
+                                    '<i class="fa fa-question-circle dp_light_gray"></i>'+
+                                '</a>';
+                    }
+                }
+            ]
         });
+
+
 
         setTimeout(function () { //https://datatables.net/forums/discussion/41587/scrolly-misaligned-table-headers-with-bootstrap
             //$($.fn.dataTable.tables( true ) ).DataTable().columns.adjust().draw();
             t.columns.adjust();
-        },200);
+        },150);
 
 
     });
@@ -27,11 +127,9 @@
 </script>
 
 
-<?php if(isset($view->roles) && sizeof($view->roles) > 0) {?>
 
-    <br/>
     <div id="empleados-table">
-            <table id="table-vehiculos" class="table table-condensed dpTable table-hover dt-responsive nowrap">
+            <table id="table-vehiculos" class="table table-condensed table-hover dt-responsive" width="100%">
                 <thead>
                 <tr>
                     <th>Rol</th>
@@ -40,45 +138,6 @@
                     <th></th>
                 </tr>
                 </thead>
-                <tbody>
-                <?php foreach ($view->roles as $ve): ?>
-                    <tr data-id="<?php echo $ve['id_user_role']; ?>">
-                        <td><?php echo $ve['nombre']; ?></td>
-                        <td><?php echo $ve['fecha_desde']; ?></td>
-                        <td><?php echo $ve['fecha_hasta']; ?></td>
-
-                        <td class="text-center">
-                            <a class="view" href="javascript:void(0);" title="ver">
-                                <i class="far fa-sticky-note dp_blue"></i>
-                            </a>&nbsp;&nbsp;
-
-                            <a class="<?php echo (PrivilegedUser::dhasPrivilege('USR_ABM', array(1)))? 'edit' : 'disabled' ?>" href="javascript:void(0);" title="editar">
-                                <i class="far fa-edit dp_blue"></i>
-                            </a>&nbsp;&nbsp;
-
-                            <a class="<?php echo ( PrivilegedUser::dhasPrivilege('USR_ABM', array(1)) )? 'delete' : 'disabled' ?>" title="borrar" href="javascript:void(0);">
-                                <i class="far fa-trash-alt dp_red"></i>
-                            </a>
-                        </td>
-                    </tr>
-                <?php endforeach; ?>
-                </tbody>
             </table>
     </div>
-
-
-
-
-<?php }else{ ?>
-
-    <br/>
-    <div class="alert alert-warning">
-        <i class="fas fa-exclamation-triangle fa-fw"></i> El usuario no tiene roles otorgados.
-    </div>
-
-<?php } ?>
-
-
-
-
 
