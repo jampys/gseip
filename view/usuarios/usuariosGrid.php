544975cf05a551743ec969ad89d616910af116b3
dario <dario@innertrip.com.ar>
2022-06-23 15:54:55 -0300

    HOTFIX-130 saved

--- view/usuarios/usuariosGrid.php
+++ view/usuarios/usuariosGrid.php
@@ -66,33 +66,24 @@
                     targets: 6,//action buttons
                     responsivePriority: 3,
                     render: function (data, type, row, meta) {
-                        let permisoAcciones = '<?php echo ( PrivilegedUser::dhasPrivilege('NC_ABM', array(1)) )? 'acciones' : 'disabled' ?>';
-                        let permisoVerificaciones = '<?php echo ( PrivilegedUser::dhasPrivilege('NC_ABM', array(1)) )? 'verificaciones' : 'disabled' ?>';
-                        let permisoEditar = '<?php echo ( PrivilegedUser::dhasPrivilege('NC_ABM', array(1)) )? 'edit' : 'disabled' ?>';
-                        let permisoEliminar = '<?php echo ( PrivilegedUser::dhasPrivilege('NC_ABM', array(1)) )? 'delete' : 'disabled' ?>';
-                        let link = 'index.php?action=nc_no_conformidad&operation=pdf&id_no_conformidad='+row.id_no_conformidad;
+                        let permisoEditar = '<?php echo ( PrivilegedUser::dhasPrivilege('USR_ABM', array(1)) )? 'edit' : 'disabled' ?>';
+                        let permisoEliminar = '<?php echo ( PrivilegedUser::dhasPrivilege('USR_ABM', array(1)) )? 'delete' : 'disabled' ?>';
                         let user_info =''; //row.user.split('@')[0]+' '+row.created_date;
-                        return '<a class="'+permisoAcciones+'" href="#" title="Acciones">'+ //si tiene permiso para ver Acciones
-                            '<i class="fas fa-th-list dp_blue"></i>'+
-                            '</a>&nbsp;&nbsp;'+
-                            '<a class="'+permisoVerificaciones+'" href="#" title="Verificaciones">'+ //si tiene permiso para ver Verificaciones
-                            '<i class="fas fa-th-list dp_blue"></i>'+
-                            '</a>&nbsp;&nbsp;'+
-                            '<a class="view" title="Ver" href="#">'+
-                            '<i class="far fa-sticky-note dp_blue"></i>'+
-                            '</a>&nbsp;&nbsp;'+
-                            '<a class="'+permisoEditar+'" href="#" title="Editar">'+ //si tiene permiso para editar
-                            '<i class="far fa-edit dp_blue"></i>'+
-                            '</a>&nbsp;&nbsp;'+
-                            '<a class="'+permisoEliminar+'" href="#" title="Eliminar">'+ //si tiene permiso para eliminar
-                            '<i class="far fa-trash-alt dp_red"></i>'+
-                            '</a>&nbsp;&nbsp;'+
-                            '<a target="_blank" href="'+link+'" title="Descargar certificado">'+
-                            '<i class="fas fa-download dp_blue"></i>'+
-                            '</a>&nbsp;&nbsp;'+
-                            '<a href="#" title="'+user_info+'" onclick="return false;">'+
-                            '<i class="fa fa-question-circle dp_light_gray"></i>'+
-                            '</a>';
+                        return '<a class="roles" href="#" title="Roles">'+
+                                    '<i class="fas fa-th-list dp_blue"></i>'+
+                                '</a>&nbsp;&nbsp;'+
+                                '<a class="view" title="Ver" href="#">'+
+                                    '<i class="far fa-sticky-note dp_blue"></i>'+
+                                '</a>&nbsp;&nbsp;'+
+                                '<a class="'+permisoEditar+'" href="#" title="Editar">'+ //si tiene permiso para editar
+                                    '<i class="far fa-edit dp_blue"></i>'+
+                                '</a>&nbsp;&nbsp;'+
+                                '<a class="'+permisoEliminar+'" href="#" title="Eliminar">'+ //si tiene permiso para eliminar
+                                    '<i class="far fa-trash-alt dp_red"></i>'+
+                                '</a>&nbsp;&nbsp;'+
+                                '<a href="#" title="'+user_info+'" onclick="return false;">'+
+                                    '<i class="fa fa-question-circle dp_light_gray"></i>'+
+                                '</a>';
                     }
                 }
             ]
